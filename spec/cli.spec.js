8103bc2e751ef7ce02c1aaa0fa9e624df8f1909d
jbavari <josh@drifty.com>
2015-04-23 16:29:04 -0600

    Pulling out the CLI tasks into their own module. Adding CLI as its own module instead of the old ionic.js file, now its cli.js which can be used as a module, where you just call its run method with args already parsed out for ease of testing

---
+++ spec/cli.spec.js
@@ -0,0 +1,83 @@
+var IonicAppLib = require('ionic-app-lib'),
+    Ionitron = require('../lib/ionic/ionitron'),
+    IonicCli = require('../lib/cli'),
+    Q = require('q'),
+    Utils = IonicAppLib.utils;
+
+describe('Cli', function() {
+
+  beforeEach(function() {
+    spyOn(IonicCli, 'processExit');
+    spyOn(IonicCli, 'printAvailableTasks');
+
+    spyOn(Utils, 'fail').andCallFake(function(err){
+      console.log(err);
+      console.log(err.stack);
+      throw err;
+    });
+  });
+
+  it('should have cli defined', function() {
+    expect(IonicCli).toBeDefined();
+  });
+
+  it('should have cli tasks defined', function() {
+    expect(IonicCli.Tasks).toBeDefined();
+  });
+
+  describe('#run', function() {
+    it('should run checkLatestVersion on run', function() {
+      var deferred = Q.defer();
+      deferred.resolve();
+      spyOn(IonicCli, 'checkLatestVersion').andReturn(deferred);
+      IonicCli.run({_: []});
+      expect(IonicCli.checkLatestVersion).toHaveBeenCalled();
+    });
+
+    it('should run ionitron when argument is passed', function() {
+      spyOn(Ionitron, 'print');
+      IonicCli.run({ionitron: true});
+      expect(Ionitron.print).toHaveBeenCalled();
+    });
+
+    it('should listen to verbosity when arg is passed', function() {
+      spyOn(IonicAppLib.events, 'on');
+      spyOn(IonicCli, 'tryBuildingTask').andReturn(false);
+      IonicCli.run({verbose: true});
+      expect(IonicAppLib.events.on).toHaveBeenCalledWith('verbose', console.log);
+    });
+
+    it('should get version when version flag passed', function() {
+      spyOn(IonicCli, 'version');
+      IonicCli.run({version: true, _: []});
+      expect(IonicCli.version).toHaveBeenCalled();
+    });
+
+    it('should call help when help argument passed', function() {
+      spyOn(IonicCli, 'printHelpLines');
+      IonicCli.run({help: true});
+      expect(IonicCli.printHelpLines).toHaveBeenCalled();
+    });
+
+    xit('should store the stats opt out if arg passed', function() {
+      throw 'TODO Write this test';
+    });
+
+    it('should print available tasks if no valid command is passed', function() {
+      spyOn(IonicCli, 'tryBuildingTask').andReturn(false);
+      IonicCli.run({});
+      expect(IonicCli.printAvailableTasks).toHaveBeenCalled();
+    });
+
+    it('should call attachErrorHandling', function() {
+      spyOn(IonicCli, 'attachErrorHandling');
+      IonicCli.run({ _: [] });
+      expect(IonicCli.attachErrorHandling).toHaveBeenCalled();
+    });
+
+    it('should run the task it finds', function() {
+      
+    });
+  });
+
+});
