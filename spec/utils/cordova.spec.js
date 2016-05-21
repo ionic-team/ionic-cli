e4f4f98950b0545bcf85976b8e66ed83a8aff2a9
Josh Thomas <josh@ionic.io>
2016-05-20 21:03:08 -0500

    chore(tests): ensure cordova utlity functions are covered by tests.

--- spec/utils/cordova.spec.js
+++ spec/utils/cordova.spec.js
@@ -1,8 +1,15 @@
 'use strict';
 
 var fs = require('fs');
-var cordovaUtils = require('../../lib/utils/cordova');
+var Q = require('q');
+var rewire = require('rewire');
+var cordovaUtils = rewire('../../lib/utils/cordova');
 var optimist = require('optimist');
+var IonicAppLib = require('ionic-app-lib');
+var Project = require('ionic-app-lib').project;
+var Serve = require('ionic-app-lib').serve;
+var ConfigXml = require('ionic-app-lib').configXml;
+var log = IonicAppLib.logging.logger;
 
 describe('isPlatformInstalled', function() {
   it('should return true if the platform directory does exist', function() {
@@ -28,7 +35,7 @@ describe('isPlatformInstalled', function() {
   });
 });
 
-describe('argePluginsInstalled', function() {
+describe('arePluginsInstalled', function() {
   it('should return true if the plugins directory does exist', function() {
     spyOn(fs, 'statSync').andCallFake(function() {
       return;
@@ -49,6 +56,192 @@ describe('argePluginsInstalled', function() {
 
     expect(result).toEqual(false);
     expect(fs.statSync).toHaveBeenCalledWith('/tmp/plugins');
+  });
+});
+
+describe('installPlatform', function() {
+  beforeEach(function() {
+    spyOn(log, 'info');
+  });
+
+  it('should call promiseExec', function(done) {
+    var installPlatform = cordovaUtils.__get__('installPlatform');
+    var promiseExecSpy = jasmine.createSpy('promiseExec');
+    promiseExecSpy.andReturn(Q(true));
+    var revertPromiseExecSpy = cordovaUtils.__set__('promiseExec', promiseExecSpy);
+
+
+    installPlatform('ios').then(function() {
+      expect(promiseExecSpy).toHaveBeenCalledWith('cordova platform add ios');
+      revertPromiseExecSpy();
+      done();
+    });
+  });
+});
+
+describe('installPlugins', function() {
+  beforeEach(function() {
+    spyOn(log, 'info');
+  });
+
+  it('should call promiseExec', function(done) {
+    var installPlugins = cordovaUtils.__get__('installPlugins');
+    var promiseExecSpy = jasmine.createSpy('promiseExec');
+    promiseExecSpy.andReturn(Q(true));
+    var revertPromiseExecSpy = cordovaUtils.__set__('promiseExec', promiseExecSpy);
+
+    installPlugins().then(function() {
+      expect(promiseExecSpy.calls[0].args[0]).toEqual('cordova plugin add --save cordova-plugin-device');
+      expect(promiseExecSpy.calls[1].args[0]).toEqual('cordova plugin add --save cordova-plugin-console');
+      expect(promiseExecSpy.calls[2].args[0]).toEqual('cordova plugin add --save cordova-plugin-whitelist');
+      expect(promiseExecSpy.calls[3].args[0]).toEqual('cordova plugin add --save cordova-plugin-splashscreen');
+      expect(promiseExecSpy.calls[4].args[0]).toEqual('cordova plugin add --save cordova-plugin-statusbar');
+      expect(promiseExecSpy.calls[5].args[0]).toEqual('cordova plugin add --save ionic-plugin-keyboard');
+      revertPromiseExecSpy();
+      done();
+    });
+  });
+});
+
+
+describe('setupLiveReload', function() {
+  var argv;
+  var baseDir;
+
+  beforeEach(function() {
+    spyOn(log, 'info');
+    argv = {};
+    baseDir = '/some/base/dir';
+  });
+  afterEach(function() {
+    argv = {};
+    baseDir = '';
+  });
+
+  it('should return options on success', function(done) {
+    argv.all = true;
+    var processCwd = '/process/current/pwd';
+    var project = { name: 'something' };
+    var loadSettings = { address: '127.0.0.1', port: 80 };
+    var serveHostValue = 'devServer value';
+    var finalOptions = {
+      port: 80,
+      address: '0.0.0.0',
+      appDirectory: baseDir,
+      runLivereload: true,
+      launchBrowser: false,
+      launchLab: false,
+      isPlatformServe: true,
+      devServer: serveHostValue
+    };
+
+    spyOn(process, 'cwd').andReturn(processCwd);
+    spyOn(Project, 'load').andReturn(project);
+    spyOn(Serve, 'loadSettings').andReturn(loadSettings);
+    spyOn(Serve, 'getAddress');
+    spyOn(Serve, 'host').andReturn(serveHostValue);
+    spyOn(Serve, 'checkPorts').andReturn(Q(true));
+    spyOn(ConfigXml, 'setConfigXml').andReturn(Q(true));
+    spyOn(Serve, 'start');
+    spyOn(Serve, 'showFinishedServeMessage');
+    var setupLiveReload = cordovaUtils.__get__('setupLiveReload');
+
+    setupLiveReload(argv, baseDir).then(function(options) {
+      expect(Project.load).toHaveBeenCalledWith(baseDir);
+      expect(Serve.loadSettings).toHaveBeenCalledWith(argv, project);
+      expect(Serve.getAddress).not.toHaveBeenCalled();
+      expect(Serve.host.calls[0].args).toEqual(['0.0.0.0', 80]);
+      expect(Serve.checkPorts).toHaveBeenCalledWith(true, 80, '0.0.0.0', finalOptions);
+      expect(ConfigXml.setConfigXml).toHaveBeenCalledWith(processCwd, { devServer: serveHostValue });
+      expect(Serve.host.calls[1].args).toEqual(['0.0.0.0', 80]);
+      expect(Serve.start).toHaveBeenCalledWith(finalOptions);
+      expect(options).toEqual(finalOptions);
+      done();
+    });
+  });
+  it('should use address if it is supplied', function(done) {
+    argv.address = '127.0.0.1';
+    var processCwd = '/process/current/pwd';
+    var project = { name: 'something' };
+    var loadSettings = { address: '127.0.0.1', port: 80 };
+    var serveHostValue = 'devServer value';
+    var finalOptions = {
+      port: 80,
+      address: '127.0.0.1',
+      appDirectory: baseDir,
+      runLivereload: true,
+      launchBrowser: false,
+      launchLab: false,
+      isPlatformServe: true,
+      devServer: serveHostValue
+    };
+
+    spyOn(process, 'cwd').andReturn(processCwd);
+    spyOn(Project, 'load').andReturn(project);
+    spyOn(Serve, 'loadSettings').andReturn(loadSettings);
+    spyOn(Serve, 'getAddress');
+    spyOn(Serve, 'host').andReturn(serveHostValue);
+    spyOn(Serve, 'checkPorts').andReturn(Q(true));
+    spyOn(ConfigXml, 'setConfigXml').andReturn(Q(true));
+    spyOn(Serve, 'start');
+    spyOn(Serve, 'showFinishedServeMessage');
+    var setupLiveReload = cordovaUtils.__get__('setupLiveReload');
+
+    setupLiveReload(argv, baseDir).then(function(options) {
+      expect(Project.load).toHaveBeenCalledWith(baseDir);
+      expect(Serve.loadSettings).toHaveBeenCalledWith(argv, project);
+      expect(Serve.getAddress).not.toHaveBeenCalled();
+      expect(Serve.host.calls[0].args).toEqual(['127.0.0.1', 80]);
+      expect(Serve.checkPorts).toHaveBeenCalledWith(true, 80, '127.0.0.1', finalOptions);
+      expect(ConfigXml.setConfigXml).toHaveBeenCalledWith(processCwd, { devServer: serveHostValue });
+      expect(Serve.host.calls[1].args).toEqual(['127.0.0.1', 80]);
+      expect(Serve.start).toHaveBeenCalledWith(finalOptions);
+      expect(options).toEqual(finalOptions);
+      done();
+    });
+  });
+  it('should return options on success and gather address if not supplied', function(done) {
+    var processCwd = '/process/current/pwd';
+    var project = { name: 'something' };
+    var loadSettings = { address: '127.0.0.1', port: 80 };
+    var serveHostValue = 'devServer value';
+    var finalOptions = {
+      port: 80,
+      address: '80.80.80.80',
+      appDirectory: baseDir,
+      runLivereload: true,
+      launchBrowser: false,
+      launchLab: false,
+      isPlatformServe: true,
+      devServer: serveHostValue
+    };
+
+    spyOn(process, 'cwd').andReturn(processCwd);
+    spyOn(Project, 'load').andReturn(project);
+    spyOn(Serve, 'loadSettings').andReturn(loadSettings);
+    spyOn(Serve, 'getAddress').andCallFake(function(options) {
+      options.address = '80.80.80.80';
+      return Q(true);
+    });
+    spyOn(Serve, 'host').andReturn(serveHostValue);
+    spyOn(Serve, 'checkPorts').andReturn(Q(true));
+    spyOn(ConfigXml, 'setConfigXml').andReturn(Q(true));
+    spyOn(Serve, 'start');
+    spyOn(Serve, 'showFinishedServeMessage');
+    var setupLiveReload = cordovaUtils.__get__('setupLiveReload');
+
+    setupLiveReload(argv, baseDir).then(function(options) {
+      expect(Project.load).toHaveBeenCalledWith(baseDir);
+      expect(Serve.loadSettings).toHaveBeenCalledWith(argv, project);
+      expect(Serve.getAddress).toHaveBeenCalled();
+      expect(Serve.host.calls[0].args).toEqual(['80.80.80.80', 80]);
+      expect(Serve.checkPorts).toHaveBeenCalledWith(true, 80, '80.80.80.80', finalOptions);
+      expect(ConfigXml.setConfigXml).toHaveBeenCalledWith(processCwd, { devServer: serveHostValue });
+      expect(Serve.host.calls[1].args).toEqual(['80.80.80.80', 80]);
+      expect(Serve.start).toHaveBeenCalledWith(finalOptions);
+      expect(options).toEqual(finalOptions);
+      done();
+    });
   });
 });
 
