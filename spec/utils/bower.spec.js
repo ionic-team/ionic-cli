9d3bb4056645e7b1410adb623ef356bdbad97b77
Josh Thomas <josh@ionic.io>
2016-05-23 08:36:48 -0500

    chore(tests): add test coverage to bower util.

---
+++ spec/utils/bower.spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var rewire = require('rewire');
+var bowerUtils = rewire('../../lib/utils/bower');
+var IonicAppLib = require('ionic-app-lib');
+
+describe('setIonicVersion function', function() {
+  it('should', function() {
+
+  });
+});
+
+describe('setAppName function', function() {
+  it('should', function() {
+
+  });
+});
+
+describe('getData function', function() {
+  it('should', function() {
+
+  });
+});
+
+describe('saveData function', function() {
+  it('should', function() {
+
+  });
+});
+
+describe('checkForBower function', function() {
+  it('should', function() {
+
+  });
+});
