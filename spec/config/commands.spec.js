7ddd42e5536d915ef412db93c8d1a7c375d7a7e4
Josh Thomas <josh@ionic.io>
2016-06-02 12:10:35 -0500

    fix(commands): add generate, prepare, address, compile to the available command list.

---
+++ spec/config/commands.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var commands = require('../../lib/config/commands');
+
+describe('orderedListOfCommands', function() {
+  var listOfCommands = commands.orderedListOfCommands.slice(0);
+
+  it('should have a corresponding command every task in ionic folder', function() {
+    var taskFileList = fs
+      .readdirSync(path.join(__dirname, '../../lib/ionic'))
+      .filter(function(file) {
+        var stat = fs.statSync(path.join(__dirname, '../../lib/ionic', file));
+        return !stat.isDirectory();
+      })
+      .map(function(file) {
+        return file.replace('.js', '');
+      })
+      .sort();
+    expect(taskFileList).toEqual(listOfCommands.sort());
+  });
+});
