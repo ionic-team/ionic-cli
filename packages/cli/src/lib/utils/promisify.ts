d203178079bcc7e81b909e2419801178a916625c
Josh Thomas <jthoms1@gmail.com>
2016-10-20 15:04:53 -0500

    feat(): add promisify utility to the cli code.

---
+++ packages/cli/src/lib/utils/promisify.ts
@@ -0,0 +1,24 @@
+export interface Promisify {
+  <T>(func: (callback: (err: any, result: T) => void) => void): () => Promise<T>;
+  <T, A1>(func: (arg1: A1, callback: (err: any, result: T) => void) => void): (arg1: A1) => Promise<T>;
+  <T, A1, A2>(func: (arg1: A1, arg2: A2, callback: (err: any, result: T) => void) => void): (arg1: A1, arg2: A2) => Promise<T>;
+  <T, A1, A2, A3>(func: (arg1: A1, arg2: A2, arg3: A3, callback: (err: any, result: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3) => Promise<T>;
+  <T, A1, A2, A3, A4>(func: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, callback: (err: any, result: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => Promise<T>;
+  <T, A1, A2, A3, A4, A5>(func: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, callback: (err: any, result: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => Promise<T>;
+}
+/**
+ * @example: const rReadFile = promisify<Buffer, string>(fs.readFile);
+ * 
+ */
+export const promisify: Promisify = function(func: any) {
+  return (...args: any[]) => {
+    return new Promise((resolve, reject) => {
+      func(...args, (err: any, response: any) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  };
+};
