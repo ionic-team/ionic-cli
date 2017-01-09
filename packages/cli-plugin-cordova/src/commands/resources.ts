6aff53b1ac1e4e2f766e8991a5b2401e3fd75be8
Josh Thomas <josh@ionic.io>
2017-01-09 09:18:10 -0600

    chore(): began breaking resources into functions

--- packages/cli-plugin-cordova/src/commands/resources.ts
+++ packages/cli-plugin-cordova/src/commands/resources.ts
@@ -25,9 +25,17 @@ interface ImageResource {
   density: string;
   platform: string;
   resType: string;
+  dest: string;
   src: string;
   nodeName: string;
   nodeAttributes: string[];
+}
+
+interface SourceImage {
+  ext: string;
+  platform: string;
+  resType: string;
+  path: string;
 }
 
 /*
@@ -68,7 +76,6 @@ export class ResourcesCommand extends Command {
 
     let configFileContents: string;
     let resourceDirContents: string[] = [];
-    let platformDirContents: string[] = [];
 
     /**
      * check that config file config.xml exists
@@ -77,32 +84,20 @@ export class ResourcesCommand extends Command {
       configFileContents = await fsReadFile(configFilePath, { encoding: 'utf8' });
     } catch (e) {
       if (e.code === 'ENOENT') {
-        this.env.log.error(`${chalk.bold(configFilePath)} does not appear to exist. Please ensure that this is a \n`
+        throw new Error(`${chalk.bold(configFilePath)} does not appear to exist. Please ensure that this is a \n`
                          + `cordova project.`);
-        return;
       }
       throw e;
     }
-
 
     /**
      * check that at least one platform has been installed
-     * TODO: should we get this from the config.xml or just the directories like app-lib
      */
-    try {
-      platformDirContents = await fsReadDir(path.join(this.env.project.directory, 'platforms'));
-    } catch (e) {
-      if (e.code === 'ENOENT') {
-        this.env.log.error('No platforms have been added.');
-        return;
-      }
-      throw e;
-    }
-
     const resourceJsonStructure = await fsReadJsonFile('../lib/resources.json');
-    const buildPlatforms = Object.keys(resourceJsonStructure).filter(platform => platformDirContents.indexOf(platform) !== -1);
+
+    const buildPlatforms = await getProjectPlatforms(resourceJsonStructure);
     if (buildPlatforms.length === 0) {
-      this.env.log.error('No platforms have been added');
+      throw new Error('No platforms have been added');
     }
 
     /**
@@ -110,34 +105,18 @@ export class ResourcesCommand extends Command {
      * so that it only has img resources that we need. Finally add src path to the
      * items that remain.
      */
-    const imgResources: ImageResource[] = flattenResourceJsonStructure(resourceJsonStructure)
+    let imgResources: ImageResource[] = flattenResourceJsonStructure(resourceJsonStructure)
       .filter((img: ImageResource) => buildPlatforms.indexOf(img.platform) !== -1)
       .filter((img: ImageResource) => resourceTypes.indexOf(img.resType) !== -1)
       .map((img: ImageResource) => ({
         ...img,
-        src: path.join(resourceDir, img.platform, img.resType, img.name)
+        dest: path.join(resourceDir, img.platform, img.resType, img.name)
       }));
-
-    // setup list of source images that will be needed
-    const srcImageSettings = [].concat.apply(buildPlatforms.map(platform => (
-      resourceTypes.map(resType => (
-        {
-          resType,
-          platform,
-          path: null
-        }
-      ))
-    )));
 
     /**
      * Create the resource directories that are needed for the images we will create
      */
-    const buildDirPromises: Promise<void>[] = imgResources
-      .map(img => path.dirname(img.src))
-      .filter((dir, index, dirNames) => dirNames.indexOf(dir) === index)
-      .map(dir => fsMkdirp(dir));
-
-    await Promise.all(buildDirPromises);
+    await createImgDestinationDirectories(imgResources);
 
     /**
      * Get orientation config data
@@ -147,45 +126,29 @@ export class ResourcesCommand extends Command {
     /**
      * Check /resources and /resources/<platform> directories for src files
      */
-    const srcDirList = buildPlatforms
-      .map(platform => (
-        {
-          platform,
-          path: path.join(resourceDir, platform)
-        }
-      ))
-      .concat({
-        platform: 'global',
-        path: resourceDir
+    const srcImagesAvailable = await getSourceImages(buildPlatforms, resourceTypes, resourceDir);
+
+    // setup list of source images that will be needed
+    imgResources = imgResources.map((imageResource: ImageResource) => {
+        var sourcePath = '';
+        srcImagesAvailable.every((sourceImage: SourceImage) => {
+          if (sourceImage.platform === imageResource.platform && sourceImage.resType === imageResource.resType) {
+            sourcePath = sourceImage.path;
+            return false;
+          }
+          if (sourceImage.platform === 'global' && sourceImage.resType === imageResource.resType) {
+            sourcePath = sourceImage.path;
+          }
+          return true;
+        });
+
+        return {
+          ...imageResource,
+          src: sourcePath
+        };
       });
-    const srcImageDirListPromises = srcDirList.map(srcImgDir => fsReadDir(srcImgDir.path));
 
-    const srcImageDirContentList = await Promise.all(srcImageDirListPromises);
-    const srcImagesAvailable = [].concat.apply(
-      srcImageDirContentList.map((srcImageDirContents, index) => (
-        srcImageDirContents
-          .map(imgName => {
-            const ext = path.extname(imgName);
 
-            return {
-              ext,
-              platform: srcDirList[index].platform,
-              resType: path.basename(imgName, ext),
-              path: path.join(srcDirList[index].path, imgName),
-            };
-          })
-          .filter(img => SUPPORTED_SOURCE_EXTENSIONS.indexOf(img.ext) !== -1)
-          .filter(img => resourceTypes.indexOf(img.resType) !== -1)
-      ))
-    );
-
-    srcImageSettings.map((sis: any) => {
-      srcImagesAvailable.filter((sia: any) => sia.platform === sis.platform && sia.resType === sis.resType);
-      return {
-        ...sis,
-        path:
-      };
-    });
 
     // loadSourceImages
     // generateResourceImages
@@ -219,4 +182,74 @@ function flattenResourceJsonStructure (jsonStructure: any): ImageResource[] {
       ))
     ))
   )));
+}
+
+/**
+ * Get all platforms based on resource/platforms directories
+ * TODO: should we get this from the config.xml or just the directories like app-lib
+ */
+async function getProjectPlatforms (jsonStructure: any): Promise<string[]> {
+  let platformDirContents: string[] = [];
+
+  try {
+    platformDirContents = await fsReadDir(path.join(this.env.project.directory, 'platforms'));
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      throw new Error('No platforms have been added.');
+    }
+    throw e;
+  }
+
+  return Object.keys(jsonStructure).filter(platform => platformDirContents.indexOf(platform) !== -1);
+}
+
+/**
+ *
+ */
+async function createImgDestinationDirectories (imgResources: ImageResource[]): Promise<void[]> {
+  const buildDirPromises: Promise<void>[] = imgResources
+    .map(img => path.dirname(img.src))
+    .filter((dir, index, dirNames) => dirNames.indexOf(dir) === index)
+    .map(dir => fsMkdirp(dir));
+
+  return Promise.all(buildDirPromises);
+}
+
+/**
+ *
+ */
+async function getSourceImages (buildPlatforms: string[], resourceTypes: string[], resourceDir: string): Promise<SourceImage[]> {
+  const srcDirList = buildPlatforms
+    .map((platform: any) => (
+      {
+        platform,
+        path: path.join(resourceDir, platform)
+      }
+    ))
+    .concat({
+      platform: 'global',
+      path: resourceDir
+    });
+
+  const srcImageDirContentList = await Promise.all(
+    srcDirList.map((srcImgDir: any) => fsReadDir(srcImgDir.path))
+  );
+
+  return [].concat.apply(
+    srcImageDirContentList.map((srcImageDirContents, index) => (
+      srcImageDirContents
+        .map((imgName: any): SourceImage => {
+          const ext = path.extname(imgName);
+
+          return {
+            ext,
+            platform: srcDirList[index].platform,
+            resType: path.basename(imgName, ext),
+            path: path.join(srcDirList[index].path, imgName),
+          };
+        })
+        .filter((img: any) => SUPPORTED_SOURCE_EXTENSIONS.indexOf(img.ext) !== -1)
+        .filter((img: any) => resourceTypes.indexOf(img.resType) !== -1)
+    ))
+  )
 }
