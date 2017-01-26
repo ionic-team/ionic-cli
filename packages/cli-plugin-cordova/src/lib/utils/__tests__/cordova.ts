80bf6a476cad20b0474b7abc1a72a4001bc30f46
Josh Thomas <josh@ionic.io>
2017-01-26 09:26:37 -0600

    chore(): write tests for filtering out options from cordova exec.

---
+++ packages/cli-plugin-cordova/src/lib/utils/__tests__/cordova.ts
@@ -0,0 +1,65 @@
+import * as cordova from '../cordova';
+
+describe('resources', () => {
+  it('filterArgumentsForCordova should return the command name and inputs if no options passed', () => {
+
+    let metadata = {
+      name: 'build',
+      description: 'Build (prepare + compile) an Ionic project for a given platform.',
+      inputs: [
+        {
+          name: 'platform',
+          description: 'the platform that you would like to build'
+        }
+      ],
+      options: [
+        {
+          name: 'nohooks',
+          description: 'Do not add default Ionic hooks for Cordova',
+          default: false,
+          aliases: []
+        }
+      ]
+    };
+    let inputs = [
+      'ios'
+    ];
+    let options = {
+      nohooks: false
+    };
+
+    const result = cordova.filterArgumentsForCordova(metadata, inputs, options);
+    expect(result).toEqual(['build', 'ios']);
+  });
+
+  it('filterArgumentsForCordova should include options if they are not on the ignored list', () => {
+
+    let metadata = {
+      name: 'build',
+      description: 'Build (prepare + compile) an Ionic project for a given platform.',
+      inputs: [
+        {
+          name: 'platform',
+          description: 'the platform that you would like to build'
+        }
+      ],
+      options: [
+        {
+          name: 'nohooks',
+          description: 'Do not add default Ionic hooks for Cordova',
+          default: false,
+          aliases: []
+        }
+      ]
+    };
+    let inputs = [
+      'ios'
+    ];
+    let options = {
+      nohooks: true
+    };
+
+    const result = cordova.filterArgumentsForCordova(metadata, inputs, options);
+    expect(result).toEqual(['build', 'ios', '--nohooks']);
+  });
+});
--- packages/cli-plugin-cordova/src/lib/utils/cordova.ts
+++ packages/cli-plugin-cordova/src/lib/utils/cordova.ts
@@ -9,7 +9,6 @@ import {
  * Filter and gather arguments from command line to be passed to Cordova
  */
 export function filterArgumentsForCordova(metadata: any, inputs: CommandLineInputs, options: CommandLineOptions): string[] {
-
   const results = normalizeOptionAliases(metadata, options);
   const args = minimistOptionsToArray(metadata, results);
 
@@ -23,13 +22,15 @@ export function filterArgumentsForCordova(metadata: any, inputs: CommandLineInpu
     '--address'
   ];
 
-  return args.filter(function(arg, index, fullList) {
+  const cleanOptions = args.filter(function(arg, index, fullList) {
 
     // Only return true if the arg is not found in the ignore Cmds list
     return ignoreCmds.every(ignoreCmd => {
       return arg.search(ignoreCmd) !== 0;
     });
   });
+
+  return [metadata.name].concat(inputs, cleanOptions);
 }
 
 export async function startAppScriptsServer(inputs: CommandLineInputs, options: CommandLineOptions): Promise<void> {
