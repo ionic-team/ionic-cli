04bd4b1dc752ff2b6ce65b201ae0387a727526d0
Josh Thomas <josh@ionic.io>
2017-02-28 22:07:14 -0600

    chore(): remove ionic2 restrictions from core and cordova.

--- packages/cli-plugin-cordova/src/index.ts
+++ packages/cli-plugin-cordova/src/index.ts
@@ -1,18 +1,10 @@
-import * as chalk from 'chalk';
-import { FatalException, getCommandMetadataList, runCommand, IonicEnvironment } from '@ionic/cli-utils';
+import { getCommandMetadataList, runCommand, IonicEnvironment } from '@ionic/cli-utils';
 import { CordovaNamespace } from './commands';
 
 export const PLUGIN_NAME = 'cordova';
 
 export async function run(envInstance: IonicEnvironment): Promise<void>  {
   const cordovaNamespace = new CordovaNamespace();
-
-  // This module does not support v1 projects.
-  let projectData = await envInstance.project.load();
-  if (!projectData.v2) {
-    throw new FatalException(`Ionic CLI v3 only supports Ionic2 projects. If you are currently working on v1 projects\n` +
-                            `then you should continue to use Ionic CLI v2 for now. ${chalk.bold('npm install -g ionic@2')}\n`);
-  }
 
   await runCommand({
     namespace: cordovaNamespace,
--- packages/cli-plugin-core/src/index.ts
+++ packages/cli-plugin-core/src/index.ts
@@ -1,16 +1,8 @@
-import * as chalk from 'chalk';
-import { FatalException, getCommandMetadataList, runCommand, IonicEnvironment } from '@ionic/cli-utils';
+import { getCommandMetadataList, runCommand, IonicEnvironment } from '@ionic/cli-utils';
 import { CoreNamespace } from './commands';
 
 export async function run(envInstance: IonicEnvironment): Promise<void>  {
   const coreNamespace = new CoreNamespace();
-
-  // This module does not support v1 projects.
-  let projectData = await envInstance.project.load();
-  if (!projectData.v2) {
-    throw new FatalException(`Ionic CLI v3 only supports Ionic2 projects. If you are currently working on v1 projects\n` +
-                            `then you should continue to use Ionic CLI v2 for now. ${chalk.bold('npm install -g ionic@2')}\n`);
-  }
 
   await runCommand({
     namespace: coreNamespace,
