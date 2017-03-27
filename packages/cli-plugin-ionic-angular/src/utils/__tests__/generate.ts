43d5527dd60722c8750d88c9812771e082705a77
Daniel Imhoff <dwieeb@gmail.com>
2017-03-27 16:29:25 -0500

    chore(tests): fix

--- packages/cli-plugin-ionic-angular/src/utils/__tests__/generate.ts
+++ packages/cli-plugin-ionic-angular/src/utils/__tests__/generate.ts
@@ -1,15 +1,13 @@
-import { getPages } from '../generate';
-
 describe('prompt', () => {
 
   it('should return a file path to the main app module', async () => {
     jest.resetModules();
     jest.mock('@ionic/cli-utils', () => ({
-      inquirer: {
+      load: jest.fn().mockReturnValue({
         prompt: jest.fn().mockReturnValueOnce({
           usage: true
         })
-      }
+      })
     }));
 
     const generate = require('../generate');
@@ -18,52 +16,48 @@ describe('prompt', () => {
       appNgModulePath: '/path/to/nowhere'
     };
 
-    const result = await generate.prompt('pipe', {}, context);
+    const result = await generate.prompt('pipe', context);
     expect(result).toEqual(context.appNgModulePath);
   });
 
   it('should return a file path to a specific ngModule', async () => {
     jest.resetModules();
     jest.mock('@ionic/cli-utils', () => ({
-      inquirer: {
-        prompt: jest.fn().mockReturnValueOnce({
-          usage: false
-        }).mockReturnValueOnce({
-          prettyName: '/path/to/ngModule',
-          whereUsed: '../../../../../../../../../path/to'
-        });
-      }
+      load: jest.fn((modulePath) => {
+        switch (modulePath) {
+          case 'inquirer':
+            return {
+              prompt: jest.fn().mockReturnValueOnce({
+                usage: false
+              }).mockReturnValueOnce({
+                prettyName: '/path/to/ngModule',
+                whereUsed: '../../../../../../../../../path/to'
+              });
+            };
+          case '@ionic/app-scripts':
+            return {
+              getNgModules: jest.fn().mockReturnValueOnce([
+                {
+                  relativePath: '/path/to/ngModule',
+                  absolutePath: '/path/to/ngModule'
+                },
+                {
+                  relativePath: '/path/to/ngModule',
+                  absolutePath: '/path/to/ngModule'
+                }
+              ]),
+              getStringPropertyValue: jest.fn().mockReturnValueOnce('.module.ts')
+            };
+        }
+      })
     }));
 
     const generate = require('../generate');
 
-    // mock appScripts
-    const appScripts = {
-      getNgModules: jest.fn(),
-      getStringPropertyValue: jest.fn()
-    };
-    appScripts.getNgModules
-      .mockReturnValueOnce([
-        {
-          relativePath: '/path/to/ngModule',
-          absolutePath: '/path/to/ngModule'
-        },
-        {
-          relativePath: '/path/to/ngModule',
-          absolutePath: '/path/to/ngModule'
-        }
-      ]);
-
-    appScripts.getStringPropertyValue
-      .mockReturnValueOnce('.module.ts');
-
-
     // mock context
-    const context = {
-      rootDir: 'my/cool/rootDir'
-    };
+    const context = { rootDir: 'my/cool/rootDir' };
 
-    const result = await generate.prompt('pipe', appScripts, context);
+    const result = await generate.prompt('pipe', context);
     expect(result).toEqual('/path/to/ngModule');
   });
 
@@ -74,7 +68,7 @@ describe('tabsPrompt', () => {
   it('should return an array', async () => {
     jest.resetModules();
     jest.mock('@ionic/cli-utils', () => ({
-      inquirer: {
+      load: jest.fn().mockReturnValue({
         prompt: jest.fn().mockReturnValueOnce({
           howMany: 2
         }).mockReturnValueOnce({
@@ -82,11 +76,11 @@ describe('tabsPrompt', () => {
         }).mockReturnValueOnce({
           tabName: 'CoolTabTwo'
         });
-      }
+      })
     }));
 
     const generate = require('../generate');
-    const result = await generate.tabsPrompt({});
+    const result = await generate.tabsPrompt();
 
     expect(result).toEqual([
       'CoolTabOne',
@@ -97,32 +91,27 @@ describe('tabsPrompt', () => {
 
 });
 
-
 describe('getPages', () => {
 
   it('should return an array', async () => {
-    // mock appScripts
-    const appScripts = {
-      getNgModules: jest.fn(),
-      getStringPropertyValue: jest.fn()
-    };
-    appScripts.getNgModules
-      .mockReturnValueOnce([
-        {
-          absolutePath: '/path/to/nowhere'
-        },
-        {
-          absolutePath: '/my/awesome/path'
-        }
-      ]);
-    appScripts.getStringPropertyValue
-      .mockReturnValueOnce('.module.ts');
+    jest.resetModules();
+    jest.mock('@ionic/cli-utils', () => ({
+      load: jest.fn().mockReturnValue({
+        getNgModules: jest.fn().mockReturnValueOnce([
+          {
+            absolutePath: '/path/to/nowhere'
+          },
+          {
+            absolutePath: '/my/awesome/path'
+          }
+        ]),
+        getStringPropertyValue: jest.fn().mockReturnValueOnce('.module.ts')
+      })
+    }));
 
-    const context = {
-      rootDir: '/my/root/dir/path'
-    };
-
-    const result = await getPages(appScripts, context);
+    const generate = require('../generate');
+    const context = { rootDir: '/my/root/dir/path' };
+    const result = await generate.getPages(context);
 
     expect(result).toEqual([{
       fileName: 'nowhere',
