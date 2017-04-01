d3e8e0f53016e19b56a7985b223fa7748a0a5193
Daniel Imhoff <dwieeb@gmail.com>
2017-03-31 20:23:35 -0500

    chore(): split out upload into lib function

--- packages/ionic/src/commands/upload.ts
+++ packages/ionic/src/commands/upload.ts
@@ -1,18 +1,12 @@
-import * as path from 'path';
-
-import * as chalk from 'chalk';
-
 import {
   Command,
   CommandLineInput,
   CommandLineInputs,
   CommandLineOptions,
   CommandMetadata,
-  DeployChannel,
-  DeployClient,
-  TaskChain,
-  createZipStream,
 } from '@ionic/cli-utils';
+
+import { upload } from '../lib/upload';
 
 @CommandMetadata({
   name: 'upload',
@@ -52,36 +46,8 @@ export class UploadCommand extends Command {
   async run(inputs: CommandLineInputs, options: CommandLineOptions): Promise<void> {
     const note = this.resolveNote(options['note']);
     const channelTag = this.resolveChannelTag(options['deploy']);
-    let channel: DeployChannel | undefined;
 
-    const tasks = new TaskChain();
-    const token = await this.env.session.getAppUserToken();
-    const deploy = new DeployClient(token, this.env.client);
-
-    if (channelTag) {
-      tasks.next('Retrieving deploy channel');
-      channel = await deploy.getChannel(channelTag);
-    }
-
-    const wwwPath = path.join(this.env.project.directory, 'www'); // TODO don't hardcode
-    const zip = createZipStream(wwwPath);
-
-    tasks.next('Requesting snapshot');
-    const snapshot = await deploy.requestSnapshotUpload({ note });
-    const uploadTask = tasks.next('Uploading snapshot');
-    await deploy.uploadSnapshot(snapshot, zip, (loaded, total) => {
-      uploadTask.progress(loaded, total);
-    });
-
-    tasks.end();
-    this.env.log.ok(`Uploaded snapshot ${chalk.bold(snapshot.uuid)}!`);
-
-    if (channel) {
-      tasks.next(`Deploying to '${channel.tag}' channel`);
-      await deploy.deploy(snapshot.uuid, channel.uuid);
-      tasks.end();
-      this.env.log.ok(`Deployed snapshot ${chalk.bold(snapshot.uuid)} to channel ${chalk.bold(channel.tag)}!`);
-    }
+    await upload(this.env, { note, channelTag });
   }
 
 }
---
+++ packages/ionic/src/lib/upload.ts
@@ -0,0 +1,47 @@
+import * as path from 'path';
+
+import * as chalk from 'chalk';
+
+import {
+  DeployChannel,
+  DeployClient,
+  DeploySnapshotRequest,
+  IonicEnvironment,
+  TaskChain,
+  createZipStream,
+} from '@ionic/cli-utils';
+
+export async function upload(env: IonicEnvironment, { note, channelTag }: { note?: string, channelTag?: string }): Promise<DeploySnapshotRequest> {
+  let channel: DeployChannel | undefined;
+
+  const tasks = new TaskChain();
+  const token = await env.session.getAppUserToken();
+  const deploy = new DeployClient(token, env.client);
+
+  if (channelTag) {
+    tasks.next('Retrieving deploy channel');
+    channel = await deploy.getChannel(channelTag);
+  }
+
+  const wwwPath = path.join(env.project.directory, 'www'); // TODO don't hardcode
+  const zip = createZipStream(wwwPath);
+
+  tasks.next('Requesting snapshot');
+  const snapshot = await deploy.requestSnapshotUpload({ note });
+  const uploadTask = tasks.next('Uploading snapshot');
+  await deploy.uploadSnapshot(snapshot, zip, (loaded, total) => {
+    uploadTask.progress(loaded, total);
+  });
+
+  tasks.end();
+  env.log.ok(`Uploaded snapshot ${chalk.bold(snapshot.uuid)}!`);
+
+  if (channel) {
+    tasks.next(`Deploying to '${channel.tag}' channel`);
+    await deploy.deploy(snapshot.uuid, channel.uuid);
+    tasks.end();
+    env.log.ok(`Deployed snapshot ${chalk.bold(snapshot.uuid)} to channel ${chalk.bold(channel.tag)}!`);
+  }
+
+  return snapshot;
+}
