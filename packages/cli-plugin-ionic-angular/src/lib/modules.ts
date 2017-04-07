f20ebc07867910c60b8b41293e43290efc230281
Daniel Imhoff <dwieeb@gmail.com>
2017-04-06 19:26:48 -0500

    chore(): semver not imported

--- packages/cli-plugin-ionic-angular/src/lib/modules.ts
+++ packages/cli-plugin-ionic-angular/src/lib/modules.ts
@@ -1,10 +1,8 @@
 import * as AppScriptsType from '@ionic/app-scripts';
 import * as inquirerType from 'inquirer';
-import * as semverType from 'semver';
 
 export function load(modulePath: '@ionic/app-scripts'): typeof AppScriptsType;
 export function load(modulePath: 'inquirer'): typeof inquirerType;
-export function load(modulePath: 'semver'): typeof semverType;
 export function load(modulePath: string): any {
   return require(modulePath);
 }
