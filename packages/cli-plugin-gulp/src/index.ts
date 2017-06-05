e7b1dd6386e101dd2db3af5e4f97cae42e4a80a9
Daniel Imhoff <dwieeb@gmail.com>
2017-06-05 17:55:06 -0500

    feat(gulp): ionic watch/build events for gulp integration
    ionic:build:before, ionic:build:after, and ionic:watch:before are now
    available as gulp tasks that hook into the CLI.

--- packages/cli-plugin-gulp/src/index.ts
+++ packages/cli-plugin-gulp/src/index.ts
@@ -1,6 +1,8 @@
 import * as path from 'path';
 
 import * as chalk from 'chalk';
+import * as gulpType from 'gulp';
+
 import {
   FatalException,
   IHookEngine,
@@ -10,16 +12,27 @@ import {
   pkgInstallPluginArgs,
   pkgManagerArgs,
   prettyPath,
+  promisify,
 } from '@ionic/cli-utils';
 
 export const name = '__NAME__';
 export const version = '__VERSION__';
 
-let _gulpInst: any;
+const BUILD_BEFORE_HOOK = 'build:before';
+const BUILD_BEFORE_TASK = `ionic:${BUILD_BEFORE_HOOK}`;
+const BUILD_AFTER_HOOK = 'build:after';
+const BUILD_AFTER_TASK = `ionic:${BUILD_AFTER_HOOK}`;
 
-async function loadGulp(env: IonicEnvironment) {
+const WATCH_BEFORE_HOOK = 'watch:before';
+const WATCH_BEFORE_TASK = `ionic:${WATCH_BEFORE_HOOK}`;
+
+const SASS_TASK = 'sass';
+
+let _gulpInst: typeof gulpType;
+
+async function loadGulp(env: IonicEnvironment): Promise<typeof gulpType | undefined> {
   if (_gulpInst || !env.project.directory) {
-    return;
+    return _gulpInst;
   }
 
   const project = await env.project.load();
@@ -69,7 +82,101 @@ async function loadGulp(env: IonicEnvironment) {
 
 export function registerHooks(hooks: IHookEngine) {
   hooks.register(name, 'plugins:init', async ({ env }) => {
+    env.events.on('watch:init', async () => {
+      const gulp = await loadGulp(env);
+
+      if (gulp) {
+        if (!gulp.hasTask(SASS_TASK)) {
+          env.log.warn(`The ${chalk.cyan(SASS_TASK)} task not found in your Gulpfile, which is used to compile SCSS files. The default Ionic Gulpfile can be downloaded from ${chalk.bold('https://github.com/ionic-team/ionic-app-base/blob/master/gulpfile.js')}`);
+        }
+
+        if (!gulp.hasTask(BUILD_BEFORE_TASK)) {
+          env.log.debug(`No ${chalk.cyan(BUILD_BEFORE_TASK)} task found in Gulpfile.`);
+        }
+
+        if (!gulp.hasTask(BUILD_AFTER_TASK)) {
+          env.log.debug(`No ${chalk.cyan(BUILD_AFTER_TASK)} task found in Gulpfile.`);
+        }
+
+        if (!gulp.hasTask(WATCH_BEFORE_TASK)) {
+          env.log.debug(`No ${chalk.cyan(WATCH_BEFORE_TASK)} task found in Gulpfile.`);
+        }
+      }
+    });
+
+    env.events.on('watch:change', async (filePath) => {
+      const gulp = await loadGulp(env);
+
+      if (gulp) {
+        const gulpStart = promisify<void, string>(gulp.start.bind(gulp));
+
+        if (gulp.hasTask(SASS_TASK)) {
+          if (path.extname(filePath) === '.scss') {
+            env.log.debug(`Invoking ${chalk.cyan(SASS_TASK)} gulp task.`);
+            try {
+              await gulpStart(SASS_TASK);
+            } catch (e) {
+              env.log.error(e);
+            }
+          }
+        }
+      }
+    });
+  });
+
+  hooks.register(name, BUILD_BEFORE_HOOK, async ({ env }) => {
     const gulp = await loadGulp(env);
+
+    if (gulp) {
+      const gulpStart = promisify<void, string>(gulp.start.bind(gulp));
+
+      if (gulp.hasTask(BUILD_BEFORE_TASK)) {
+        env.log.debug(`Invoking ${chalk.cyan(BUILD_BEFORE_TASK)} gulp task.`);
+        try {
+          await gulpStart(BUILD_BEFORE_TASK);
+        } catch (e) {
+          env.log.error(`Error occurred during ${chalk.cyan(BUILD_BEFORE_TASK)} gulp task. Use ${chalk.green('--verbose')} to show details.`);
+          throw e;
+        }
+      }
+    }
+  });
+
+  hooks.register(name, BUILD_AFTER_HOOK, async ({ env }) => {
+    const gulp = await loadGulp(env);
+
+    if (gulp) {
+      const gulpStart = promisify<void, string>(gulp.start.bind(gulp));
+
+      if (gulp.hasTask(BUILD_AFTER_TASK)) {
+        env.log.debug(`Invoking ${chalk.cyan(BUILD_AFTER_TASK)} gulp task.`);
+
+        try {
+          await gulpStart(BUILD_AFTER_TASK);
+        } catch (e) {
+          env.log.error(`Error occurred during ${chalk.cyan(BUILD_AFTER_TASK)} gulp task. Use ${chalk.green('--verbose')} to show details.`);
+          throw e;
+        }
+      }
+    }
+  });
+
+  hooks.register(name, WATCH_BEFORE_HOOK, async ({ env }) => {
+    const gulp = await loadGulp(env);
+
+    if (gulp) {
+      const gulpStart = promisify<void, string>(gulp.start.bind(gulp));
+
+      if (gulp.hasTask(WATCH_BEFORE_TASK)) {
+        env.log.debug(`Invoking ${chalk.cyan(WATCH_BEFORE_TASK)} gulp task.`);
+        try {
+          await gulpStart(WATCH_BEFORE_TASK);
+        } catch (e) {
+          env.log.error(`Error occurred during ${chalk.cyan(WATCH_BEFORE_TASK)} gulp task. Use ${chalk.green('--verbose')} to show details.`);
+          throw e;
+        }
+      }
+    }
   });
 
   hooks.register(name, 'command:info', async ({ env }) => {
