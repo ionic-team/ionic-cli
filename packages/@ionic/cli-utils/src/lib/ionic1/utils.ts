d89ac0982a14a624cbd519d73561692901492cd8
Daniel Imhoff <dwieeb@gmail.com>
2017-08-14 16:42:00 -0500

    fix(info): add additional attempt to get ionic 1 version from bower.json
    addresses: https://github.com/ionic-team/ionic-cli/issues/2613#issuecomment-322122902

--- packages/@ionic/cli-utils/src/lib/ionic1/utils.ts
+++ packages/@ionic/cli-utils/src/lib/ionic1/utils.ts
@@ -7,12 +7,36 @@ import { IonicEnvironment } from '../../definitions';
 import { fsReadJsonFile } from '../utils/fs';
 
 export async function getIonic1Version(env: IonicEnvironment): Promise<string | undefined> {
+  const { prettyPath } = await import('../utils/format');
+
   const ionicVersionFilePath = path.resolve(env.project.directory, 'www', 'lib', 'ionic', 'version.json'); // TODO
+  const bowerJsonPath = path.resolve(env.project.directory, 'bower.json');
 
   try {
-    const ionicVersionJson = await fsReadJsonFile(ionicVersionFilePath);
-    return ionicVersionJson['version'];
+    try {
+      const ionicVersionJson = await fsReadJsonFile(ionicVersionFilePath);
+      return ionicVersionJson['version'];
+    } catch (e) {
+      env.log.warn(`Error with ${chalk.bold(prettyPath(ionicVersionFilePath))} file: ${e}, trying ${chalk.bold(prettyPath(bowerJsonPath))}.`);
+
+      const bowerJson = await env.project.loadBowerJson();
+      let ionicEntry = bowerJson.dependencies && typeof bowerJson.dependencies['ionic'] === 'string' ? bowerJson.dependencies['ionic'] : undefined;
+
+      if (!ionicEntry) {
+        ionicEntry = bowerJson.devDependencies && typeof bowerJson.devDependencies['ionic'] === 'string' ? bowerJson.devDependencies['ionic'] : undefined;
+      }
+
+      if (!ionicEntry) {
+        return;
+      }
+
+      const m = ionicEntry.match(/.+#(.+)/);
+
+      if (m && m[1]) {
+        return m[1];
+      }
+    }
   } catch (e) {
-    env.log.error(`Error with ${chalk.bold(ionicVersionFilePath)} file: ${e}`);
+    env.log.error(`Error with ${chalk.bold(prettyPath(bowerJsonPath))} file: ${e}`);
   }
 }
