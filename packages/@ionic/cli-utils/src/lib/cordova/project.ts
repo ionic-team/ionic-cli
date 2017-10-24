d2e2b2636794f539d3cafcbf9cc0ded56ae6fc0b
Daniel Imhoff <dwieeb@gmail.com>
2017-10-24 15:52:26 -0500

    fix(cordova): only return known platforms

--- packages/@ionic/cli-utils/src/lib/cordova/project.ts
+++ packages/@ionic/cli-utils/src/lib/cordova/project.ts
@@ -2,15 +2,19 @@ import * as path from 'path';
 
 import chalk from 'chalk';
 
-import { IonicEnvironment } from '../../definitions';
+import { IonicEnvironment, KnownPlatform } from '../../definitions';
 
 import { FatalException } from '../errors';
 import { readDir } from '@ionic/cli-framework/utils/fs';
 
+function isKnownPlatform(platform: string): platform is KnownPlatform {
+  return ['android', 'ios'].includes(platform);
+}
+
 export async function getPlatforms(projectDir: string): Promise<string[]> {
   const platformsDir = path.resolve(projectDir, 'platforms');
   const dirContents = await readDir(platformsDir);
-  return dirContents.filter(f => f && f !== 'platforms.json');
+  return dirContents.filter(f => f && isKnownPlatform(f));
 }
 
 export async function installPlatform(env: IonicEnvironment, platform: string): Promise<void> {
