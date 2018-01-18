8635f53d62097e30cfa827d62d1564a07ab1621c
Daniel Imhoff <dwieeb@gmail.com>
2018-01-18 16:10:33 -0600

    chore(deps): cleanup, update

--- packages/@ionic/cli-utils/src/lib/utils/archive.ts
+++ packages/@ionic/cli-utils/src/lib/utils/archive.ts
@@ -1,12 +1,6 @@
-import * as archiverType from 'archiver';
 import * as tarType from 'tar';
 
 export type TarExtractOptions = tarType.ExtractOptions & tarType.FileOptions;
-
-export async function createArchive(format: 'zip' | 'tar'): Promise<archiverType.Archiver> {
-  const archiver = await import('archiver');
-  return archiver(format);
-}
 
 export async function createTarExtraction(opts?: TarExtractOptions): Promise<NodeJS.WritableStream> {
   const tar = await import('tar');
