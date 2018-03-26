a3f12accedde982d84f99d639ac3d2aedd7f0c01
Daniel Imhoff <dwieeb@gmail.com>
2018-03-26 17:56:00 -0500

    chore(deps): use DT types for wrap-ansi

--- types/wrap-ansi.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'wrap-ansi' {
-  function wrapAnsi(input: string, columns: number, options?: { hard?: boolean; trim?: boolean; wordWrap?: boolean }): string;
-
-  namespace wrapAnsi {}
-
-  export = wrapAnsi;
-}
