b26143e2ffd6e608901e12386b18f64469674bee
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2018-04-03 11:20:23 +0900

    feat(ailments): add common version check ailment class (#3054)

--- packages/@ionic/cli-utils/src/lib/project/angular/ailments.ts
+++ packages/@ionic/cli-utils/src/lib/project/angular/ailments.ts
@@ -8,11 +8,65 @@ import { pkgFromRegistry, pkgManagerArgs } from '../../utils/npm';
 import { Project as AngularProject } from './';
 
 export function registerAilments(registry: IAilmentRegistry, deps: AutomaticallyTreatableAngularAilmentDeps) {
-  registry.register(new IonicForAngularUpdateAvailable(deps));
-  registry.register(new IonicForAngularMajorUpdateAvailable(deps));
+  // for @ionic/angular
+  registry.register(new UpdateAvailable(deps, {
+    id: 'ionic-for-angular-update-available',
+    pkgName: '@ionic/angular',
+    treatmentVisitURL: ['https://github.com/ionic-team/ionic/releases'],
+  }));
+  registry.register(new MajorUpdateAvailable(deps, {
+    id: 'ionic-for-angular-major-update-available',
+    pkgName: '@ionic/angular',
+    treatmentVisitURL: ['https://blog.ionicframework.com', 'https://github.com/ionic-team/ionic/releases'],
+  }));
 
-  // TODO: @ionic/core update available
-  // TODO: Angular CLI update available
+  // @ionic/schematics-angular
+  registry.register(new UpdateAvailable(deps, {
+    id: 'ionic-schematics-angular-update-available',
+    pkgName: '@ionic/schematics-angular',
+    treatmentVisitURL: ['https://github.com/ionic-team/ionic/releases'],
+  }));
+  registry.register(new MajorUpdateAvailable(deps, {
+    id: 'ionic-schematics-angular-major-update-available',
+    pkgName: '@ionic/schematics-angular',
+    treatmentVisitURL: ['https://blog.ionicframework.com', 'https://github.com/ionic-team/ionic/releases'],
+  }));
+
+  // @angular/cli
+  registry.register(new UpdateAvailable(deps, {
+    id: 'angular-cli-update-available',
+    pkgName: '@angular/cli',
+    treatmentVisitURL: ['https://github.com/angular/angular-cli/releases'],
+  }));
+  registry.register(new MajorUpdateAvailable(deps, {
+    id: 'angular-cli-major-update-available',
+    pkgName: '@angular/cli',
+    treatmentVisitURL: ['https://blog.angular.io', 'https://github.com/angular/angular-cli/releases'],
+  }));
+
+  // @angular-devkit/core
+  registry.register(new UpdateAvailable(deps, {
+    id: 'angular-devkit-core-update-available',
+    pkgName: '@angular-devkit/core',
+    treatmentVisitURL: ['https://github.com/angular/devkit/releases'],
+  }));
+  registry.register(new MajorUpdateAvailable(deps, {
+    id: 'angular-devkit-core-major-update-available',
+    pkgName: '@angular-devkit/core',
+    treatmentVisitURL: ['https://blog.angular.io', 'https://github.com/angular/devkit/releases'],
+  }));
+
+  // @angular-devkit/schematics
+  registry.register(new UpdateAvailable(deps, {
+    id: 'angular-devkit-schematics-update-available',
+    pkgName: '@angular-devkit/schematics',
+    treatmentVisitURL: ['https://github.com/angular/devkit/releases'],
+  }));
+  registry.register(new MajorUpdateAvailable(deps, {
+    id: 'angular-devkit-schematics-major-update-available',
+    pkgName: '@angular-devkit/schematics',
+    treatmentVisitURL: ['https://blog.angular.io', 'https://github.com/angular/devkit/releases'],
+  }));
 }
 
 interface AngularAilmentDeps extends AilmentDeps {
@@ -23,11 +77,39 @@ export interface AutomaticallyTreatableAngularAilmentDeps extends AutomaticallyT
   project: AngularProject;
 }
 
+export interface AilmentParams {
+  id: string;
+  pkgName: string;
+  treatmentVisitURL: string[];
+}
+
 abstract class AngularAilment extends Ailment {
   protected readonly project: AngularProject;
+  protected readonly pkgParams: AilmentParams;
+  currentVersion?: string;
+  latestVersion?: string;
 
-  constructor(deps: AngularAilmentDeps) {
+  constructor(deps: AngularAilmentDeps, pkgParams: AilmentParams) {
     super(deps);
+    this.pkgParams = pkgParams;
+    this.pkgParams.treatmentVisitURL = this.pkgParams.treatmentVisitURL.map(url => chalk.bold(url));
+  }
+
+  async getVersionPair(): Promise<[string, string]> {
+    const config = await this.config.load();
+    const { npmClient } = config;
+
+    if (!this.currentVersion || !this.latestVersion) {
+      this.currentVersion = await this.project.getPackageVersion(this.pkgParams.pkgName);
+      const pkg = await pkgFromRegistry(npmClient, { pkg: this.pkgParams.pkgName });
+      this.latestVersion = pkg ? pkg.version : undefined;
+    }
+
+    if (!this.currentVersion || !this.latestVersion) {
+      return ['0.0.0', '0.0.0'];
+    }
+
+    return [ this.currentVersion, this.latestVersion ];
   }
 }
 
@@ -39,34 +121,15 @@ abstract class AngularAilment extends Ailment {
 //   }
 // }
 
-class IonicForAngularUpdateAvailable extends AngularAilment {
-  id = 'ionic-for-angular-update-available';
-  currentVersion?: string;
-  latestVersion?: string;
-
-  async getVersionPair(): Promise<[string, string]> {
-    const config = await this.config.load();
-    const { npmClient } = config;
-
-    if (!this.currentVersion || !this.latestVersion) {
-      this.currentVersion = await this.project.getPackageVersion('@ionic/angular');
-      const pkg = await pkgFromRegistry(npmClient, { pkg: '@ionic/angular' });
-      this.latestVersion = pkg ? pkg.version : undefined;
-    }
-
-    if (!this.currentVersion || !this.latestVersion) {
-      return ['0.0.0', '0.0.0'];
-    }
-
-    return [ this.currentVersion, this.latestVersion ];
-  }
+class UpdateAvailable extends AngularAilment {
+  id = this.pkgParams.id;
 
   async getMessage() {
     const [ currentVersion, latestVersion ] = await this.getVersionPair();
 
     return (
-      `Update available for ${chalk.bold('@ionic/angular')}.\n` +
-      `An update is available for ${chalk.bold('@ionic/angular')} (${chalk.cyan(currentVersion)} => ${chalk.cyan(latestVersion)}).\n`
+      `Update available for ${chalk.bold(this.pkgParams.pkgName)}.\n` +
+      `An update is available for ${chalk.bold(this.pkgParams.pkgName)} (${chalk.cyan(currentVersion)} => ${chalk.cyan(latestVersion)}).\n`
     ).trim();
   }
 
@@ -81,44 +144,27 @@ class IonicForAngularUpdateAvailable extends AngularAilment {
     const config = await this.config.load();
     const { npmClient } = config;
     const [ , latestVersion ] = await this.getVersionPair();
-    const args = await pkgManagerArgs(npmClient, { command: 'install', pkg: `@ionic/angular@${latestVersion ? latestVersion : 'latest'}` });
+    const args = await pkgManagerArgs(npmClient, { command: 'install', pkg: this.pkgParams.pkgName + `@${latestVersion ? latestVersion : 'latest'}` });
 
     return [
-      { name: `Visit ${chalk.bold('https://github.com/ionic-team/ionic/releases')} for each upgrade's instructions` },
+      { name: `Visit ${this.pkgParams.treatmentVisitURL.join(' and ')} for each upgrade's instructions` },
       { name: `If no instructions, run: ${chalk.green(args.join(' '))}` },
       { name: `Watch for npm warnings about peer dependencies--they may need manual updating` },
     ];
   }
 }
 
-class IonicForAngularMajorUpdateAvailable extends AngularAilment {
-  id = 'ionic-for-angular-major-update-available';
+class MajorUpdateAvailable extends AngularAilment {
+  id = this.pkgParams.id;
   currentVersion?: string;
   latestVersion?: string;
-
-  async getVersionPair(): Promise<[string, string]> {
-    const config = await this.config.load();
-    const { npmClient } = config;
-
-    if (!this.currentVersion || !this.latestVersion) {
-      this.currentVersion = await this.project.getPackageVersion('@ionic/angular');
-      const pkg = await pkgFromRegistry(npmClient, { pkg: '@ionic/angular' });
-      this.latestVersion = pkg ? pkg.version : undefined;
-    }
-
-    if (!this.currentVersion || !this.latestVersion) {
-      return ['0.0.0', '0.0.0'];
-    }
-
-    return [ this.currentVersion, this.latestVersion ];
-  }
 
   async getMessage() {
     const [ currentVersion, latestVersion ] = await this.getVersionPair();
 
     return (
-      `Major update available for ${chalk.bold('@ionic/angular')}.\n` +
-      `A major update is available for ${chalk.bold('@ionic/angular')} (${chalk.cyan(currentVersion)} => ${chalk.cyan(latestVersion)}).\n`
+      `Major update available for ${chalk.bold(this.pkgParams.pkgName)}.\n` +
+      `A major update is available for ${chalk.bold(this.pkgParams.pkgName)} (${chalk.cyan(currentVersion)} => ${chalk.cyan(latestVersion)}).\n`
     ).trim();
   }
 
@@ -131,7 +177,7 @@ class IonicForAngularMajorUpdateAvailable extends AngularAilment {
 
   async getTreatmentSteps() {
     return [
-      { name: `Visit ${chalk.bold('https://blog.ionicframework.com')} and ${chalk.bold('https://github.com/ionic-team/ionic/releases')} for upgrade instructions` },
+      { name: `Visit ${this.pkgParams.treatmentVisitURL.join(' and ')} for upgrade instructions` },
     ];
   }
 }
