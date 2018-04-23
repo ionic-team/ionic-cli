f797c577f3e4ec7b523616054afbb23117d63654
Daniel Imhoff <dwieeb@gmail.com>
2018-04-23 17:47:49 -0500

    feat(doctor): prettier & more helpful list command

--- packages/ionic/src/commands/doctor/list.ts
+++ packages/ionic/src/commands/doctor/list.ts
@@ -1,6 +1,9 @@
 import chalk from 'chalk';
 
-import { CommandLineInputs, CommandLineOptions, CommandMetadata } from '@ionic/cli-utils';
+import { columnar } from '@ionic/cli-framework/utils/format';
+import { strcmp } from '@ionic/cli-framework/utils/string';
+
+import { CommandLineInputs, CommandLineOptions, CommandMetadata, isTreatableAilment } from '@ionic/cli-utils';
 import { Command } from '@ionic/cli-utils/lib/command';
 
 export class DoctorListCommand extends Command {
@@ -8,14 +11,41 @@ export class DoctorListCommand extends Command {
     return {
       name: 'list',
       type: 'project',
-      summary: 'List all issue identifiers',
+      summary: 'List all issues and their identifiers',
+      description: `
+If an issue is marked as ${chalk.bold('treatable')}, then ${chalk.green('ionic doctor treat')} can attempt to fix it. Issues marked as ${chalk.bold('ignored')} will not be detected in either ${chalk.green('ionic doctor check')} or ${chalk.green('ionic doctor treat')}.
+
+You can flip whether an issue is ignored or not by using ${chalk.green('ionic config set -g doctor.issues.<issue-id>.ignored true/false')}, where ${chalk.green('<issue-id>')} matches an ID listed with this command.
+      `,
     };
   }
 
   async run(inputs: CommandLineInputs, options: CommandLineOptions): Promise<void> {
     const registry = await this.env.project.getAilmentRegistry(this.env);
-    const ailmentIds = registry.ailments.map(ailment => ailment.id);
+    const config = await this.env.config.load();
 
-    this.env.log.rawmsg(ailmentIds.map(id => chalk.green(id)).join('\n'));
+    const rows = await Promise.all(registry.ailments.map(async ailment => {
+      const issueConfig = config.doctor.issues[ailment.id];
+      const ignored = issueConfig && issueConfig.ignored;
+
+      const meta: string[] = [];
+
+      if (ignored) {
+        meta.push(chalk.red('ignored'));
+      }
+
+      if (isTreatableAilment(ailment)) {
+        meta.push('treatable');
+      }
+
+      return [
+        chalk.green(ailment.id),
+        meta.join(', '),
+      ];
+    }));
+
+    rows.sort((row1, row2) => strcmp(row1[0], row2[0]));
+
+    this.env.log.rawmsg(columnar(rows, { headers: ['id', 'meta'] }));
   }
 }
