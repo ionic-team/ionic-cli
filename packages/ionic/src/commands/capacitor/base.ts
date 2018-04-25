662c531f8d913eea457ef37c1300e076227e8821
Daniel Imhoff <dwieeb@gmail.com>
2018-04-25 10:24:31 -0500

    fix(capacitor): friendlier install prompts

--- packages/ionic/src/commands/capacitor/base.ts
+++ packages/ionic/src/commands/capacitor/base.ts
@@ -1,4 +1,6 @@
-import { CommandInstanceInfo, IShellRunOptions } from '@ionic/cli-utils';
+import chalk from 'chalk';
+
+import { CommandInstanceInfo } from '@ionic/cli-utils';
 import { Command } from '@ionic/cli-utils/lib/command';
 import { FatalException } from '@ionic/cli-utils/lib/errors';
 import { runCommand } from '@ionic/cli-utils/lib/executor';
@@ -19,29 +21,59 @@ export abstract class CapacitorCommand extends Command {
     runinfo;
   }
 
-  async runCapacitor(argList: string[], { fatalOnNotFound = false, truncateErrorOutput = 5000, ...options }: IShellRunOptions = {}): Promise<void> {
+  async runCapacitor(argList: string[]): Promise<void> {
     const { ERROR_SHELL_COMMAND_NOT_FOUND } = await import('@ionic/cli-utils/lib/shell');
+
     try {
       this.env.close();
-      await this.env.shell.run('capacitor', argList, { fatalOnNotFound, truncateErrorOutput, stdio: 'inherit', ...options });
+      await this._runCapacitor(argList);
       this.env.open();
     } catch (e) {
       this.env.open();
       if (e === ERROR_SHELL_COMMAND_NOT_FOUND) {
-        throw new FatalException(
-          `Unable to run npx, the new command execution system in NPM.
-          This means your Node version is too old. Capacitor requires Node 8.6.0 or greater,
-          and npm version 5.6.0 or greater.
-          Visit https://nodejs.org/en/ and install the latest LTS version`
-        );
-      }
-
-      if (options.fatalOnError) {
+        const pkg = '@capacitor/cli';
+        const requiredMsg = `The Capacitor CLI is required for Capacitor projects.`;
         this.env.log.nl();
-        this.env.log.error('Please see error above to fix problems running command');
-      }
+        this.env.log.info(`Looks like ${chalk.green(pkg)} isn't installed in this project.\n` + requiredMsg);
 
-      throw e;
+        const installed = await this.promptToInstallCapacitor();
+
+        if (!installed) {
+          throw new FatalException(`${chalk.green(pkg)} is required for Capacitor projects.`);
+        }
+
+        await this._runCapacitor(argList);
+      } else {
+        throw e;
+      }
     }
+  }
+
+  private async promptToInstallCapacitor(): Promise<boolean> {
+    const { pkgManagerArgs } = await import('@ionic/cli-utils/lib/utils/npm');
+    const config = await this.env.config.load();
+    const { npmClient } = config;
+
+    const pkg = '@capacitor/cli';
+    const [ manager, ...managerArgs ] = await pkgManagerArgs(npmClient, { pkg, command: 'install', saveDev: true });
+
+    const confirm = await this.env.prompt({
+      name: 'confirm',
+      message: `Install ${chalk.green(pkg)}?`,
+      type: 'confirm',
+    });
+
+    if (!confirm) {
+      this.env.log.warn(`Not installing--here's how to install manually: ${chalk.green(`${manager} ${managerArgs.join(' ')}`)}`);
+      return false;
+    }
+
+    await this.env.shell.run(manager, managerArgs, { cwd: this.env.project.directory });
+
+    return true;
+  }
+
+  private async _runCapacitor(argList: string[]) {
+    await this.env.shell.run('capacitor', argList, { fatalOnNotFound: false, truncateErrorOutput: 5000, stdio: 'inherit' });
   }
 }
