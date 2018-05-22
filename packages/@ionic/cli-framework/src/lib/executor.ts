62c0c3a97da2dc6e2e8219a130592c63743f5632
Daniel Imhoff <dwieeb@gmail.com>
2018-05-22 18:15:50 -0500

    feat(executor): ipc mode (wip)

--- packages/@ionic/cli-framework/src/lib/executor.ts
+++ packages/@ionic/cli-framework/src/lib/executor.ts
@@ -1,19 +1,35 @@
+import { EventEmitter } from 'events';
+
 import * as lodash from 'lodash';
 
 import { CommandInstanceInfo, CommandMetadata, CommandMetadataInput, CommandMetadataOption, ICommand, IExecutor, INamespace, NamespaceLocateResult } from '../definitions';
-import { isCommand } from '../guards';
+import { isCommand, isNamespace } from '../guards';
 
 import { Colors, DEFAULT_COLORS } from './colors';
 import { Command, Namespace } from './command';
-import { CommandSchemaHelpFormatter, CommandStringHelpFormatter, NamespaceSchemaHelpFormatter, NamespaceStringHelpFormatter } from './help';
+import { CommandHelpSchema, CommandSchemaHelpFormatter, CommandStringHelpFormatter, HelpFormatter, NamespaceHelpSchema, NamespaceSchemaHelpFormatter, NamespaceStringHelpFormatter } from './help';
 import { metadataToParseArgsOptions, parseArgs, stripOptions } from './options';
-import { isNamespace } from '../guards';
+import { RPC, RPCProcess } from '../utils/ipc'; // type import
 
-export abstract class AbstractExecutor<C extends ICommand<C, N, M, I, O>, N extends INamespace<C, N, M, I, O>, M extends CommandMetadata<I, O>, I extends CommandMetadataInput, O extends CommandMetadataOption> implements IExecutor<C, N, M, I, O> {
+export type HelpRPC<S extends CommandHelpSchema | NamespaceHelpSchema> = RPC<'help', [ReadonlyArray<string>], S>;
+
+export interface ExecutorOperations {
+  readonly RPC: string;
+}
+
+export const EXECUTOR_OPS: ExecutorOperations = Object.freeze({
+  RPC: '📡',
+});
+
+export abstract class AbstractExecutor<C extends ICommand<C, N, M, I, O>, N extends INamespace<C, N, M, I, O>, M extends CommandMetadata<I, O>, I extends CommandMetadataInput, O extends CommandMetadataOption> extends EventEmitter implements IExecutor<C, N, M, I, O> {
   abstract readonly namespace: N;
 
   abstract execute(argv: ReadonlyArray<string>, env: { [key: string]: string; }): Promise<void>;
   abstract run(command: C, cmdargs: ReadonlyArray<string>, runinfo?: Partial<CommandInstanceInfo<C, N, M, I, O>>): Promise<void>;
+}
+
+export interface BaseExecutorFormatHelpOptions {
+  format?: 'terminal' | 'json';
 }
 
 export interface BaseExecutorDeps<C extends ICommand<C, N, M, I, O>, N extends INamespace<C, N, M, I, O>, M extends CommandMetadata<I, O>, I extends CommandMetadataInput, O extends CommandMetadataOption> {
@@ -21,6 +37,11 @@ export interface BaseExecutorDeps<C extends ICommand<C, N, M, I, O>, N extends I
   readonly colors?: Colors;
   readonly stdout?: NodeJS.WriteStream;
   readonly stderr?: NodeJS.WriteStream;
+}
+
+export interface BaseExecutor<C extends ICommand<C, N, M, I, O>, N extends INamespace<C, N, M, I, O>, M extends CommandMetadata<I, O>, I extends CommandMetadataInput, O extends CommandMetadataOption> extends AbstractExecutor<C, N, M, I, O> {
+  on(event: 'operation-rpc', callback: (rpc: RPCProcess) => void): this;
+  emit(event: 'operation-rpc', rpc: RPCProcess): boolean;
 }
 
 export class BaseExecutor<C extends ICommand<C, N, M, I, O>, N extends INamespace<C, N, M, I, O>, M extends CommandMetadata<I, O>, I extends CommandMetadataInput, O extends CommandMetadataOption> extends AbstractExecutor<C, N, M, I, O> {
@@ -49,11 +70,16 @@ export class BaseExecutor<C extends ICommand<C, N, M, I, O>, N extends INamespac
    * @param env Environment variables for this execution.
    */
   async execute(argv: ReadonlyArray<string>, env: { [key: string]: string; }): Promise<void> {
+    if (argv[0] === EXECUTOR_OPS.RPC) {
+      return this.rpc();
+    }
+
     const parsedArgs = stripOptions(argv, { includeSeparated: false });
     const location = await this.namespace.locate(parsedArgs);
 
     if (argv.find(arg => arg === '--help' || arg === '-?') || isNamespace(location.obj)) {
-      this.stdout.write(await this.formatHelp(location, argv));
+      const cmdoptions = parseArgs([...argv]);
+      this.stdout.write(await this.formatHelp(location, { format: cmdoptions['json'] ? 'json' : 'terminal' }));
     } else {
       const cmd = location.obj;
       const cmdargs = lodash.drop(argv, location.path.length - 1);
@@ -71,18 +97,45 @@ export class BaseExecutor<C extends ICommand<C, N, M, I, O>, N extends INamespac
     await command.run(cmdinputs, cmdoptions, runinfo);
   }
 
-  async formatHelp(location: NamespaceLocateResult<C, N, M, I, O>, argv: ReadonlyArray<string>): Promise<string> {
-    const cmdoptions = parseArgs([...argv]);
+  async formatHelp(location: NamespaceLocateResult<C, N, M, I, O>, { format = 'terminal' }: BaseExecutorFormatHelpOptions = {}): Promise<string> {
+    let formatter: HelpFormatter;
 
     if (isCommand(location.obj)) {
       const options = { location, command: location.obj };
-      const formatter = cmdoptions['json'] ? new CommandSchemaHelpFormatter(options) : new CommandStringHelpFormatter(options);
-      return formatter.format();
+      formatter = format === 'json' ? new CommandSchemaHelpFormatter(options) : new CommandStringHelpFormatter(options);
     } else {
       const options = { location, namespace: location.obj };
-      const formatter = cmdoptions['json'] ? new NamespaceSchemaHelpFormatter(options) : new NamespaceStringHelpFormatter(options);
-      return formatter.format();
+      formatter = format === 'json' ? new NamespaceSchemaHelpFormatter(options) : new NamespaceStringHelpFormatter(options);
     }
+
+    return formatter.format();
+  }
+
+  /**
+   * Initiate RPC operation.
+   *
+   * This means the CLI has been executed by a parent Node process with an IPC
+   * channel, allowing request/response communication via RPC.
+   */
+  async rpc(): Promise<void> {
+    const { RPCProcess } = await import('../utils/ipc');
+    const metadata = await this.namespace.getMetadata();
+
+    const rpc = new RPCProcess({ name: metadata.name });
+
+    rpc.register<HelpRPC<CommandHelpSchema | NamespaceHelpSchema>>('help', async ([cmdpath]) => {
+      const location = await this.namespace.locate(cmdpath);
+
+      const formatter = isCommand(location.obj)
+        ? new CommandSchemaHelpFormatter({ location, command: location.obj })
+        : new NamespaceSchemaHelpFormatter({ location, namespace: location.obj });
+
+      return formatter.serialize();
+    });
+
+    this.emit('operation-rpc', rpc);
+
+    rpc.start(process);
   }
 }
 
---
+++ packages/@ionic/cli-framework/src/utils/ipc.ts
@@ -0,0 +1,196 @@
+import * as fs from 'fs';
+import { ChildProcess } from 'child_process';
+
+import * as Debug from 'debug';
+
+import { ERROR_IPC_UNKNOWN_PROCEDURE, IPCError } from '../errors';
+import { fork } from './shell';
+
+const debug = Debug('ionic:cli-framework:utils:ipc');
+
+export interface RPCRequest<P extends string, A extends any[]> {
+  type: 'rpc-request';
+  id: string;
+  procedure: P;
+  args: A;
+}
+
+export interface RPCResponse<R extends RPCRequest<any, any>, D extends object> {
+  type: 'rpc-response';
+  id: string;
+  procedure: R['procedure'];
+  request: R;
+  err?: any;
+  data: D;
+}
+
+export type RPC<P extends string, A extends any[], D extends object> = RPCResponse<RPCRequest<P, A>, D>;
+
+export interface RPCProcessOptions {
+  readonly name?: string;
+  readonly timeout?: number;
+}
+
+export class RPCProcess {
+  readonly name: string;
+  readonly timeout: number;
+  protected responseProcedures: Map<string, (args: any[]) => Promise<any>> = new Map();
+  protected proc?: ChildProcess;
+
+  constructor({ name = 'unnamed', timeout = 5000 }: RPCProcessOptions = {}) {
+    this.name = name;
+    this.timeout = timeout;
+  }
+
+  start(proc: ChildProcess | NodeJS.Process): void {
+    if (this.proc) {
+      throw new IPCError('RPC process already started.');
+    }
+
+    const p = <ChildProcess>proc;
+
+    if (!p.send) {
+      throw new IPCError('Cannot use proc: `send()` undefined.');
+    }
+
+    this.proc = p;
+
+    p.on('message', async (msg: any) => {
+      if (isRPCRequest(msg)) {
+        debug('%s: Received RPC request: %O', this.name, msg);
+        const fn = this.responseProcedures.get(msg.procedure);
+        let err: any;
+        let data: any;
+
+        if (fn) {
+          try {
+            data = await fn(msg.args);
+          } catch (e) {
+            err = e;
+          }
+        } else {
+          err = new IPCError(`Unknown procedure: ${msg.procedure}`);
+          err.code = ERROR_IPC_UNKNOWN_PROCEDURE;
+        }
+
+        const response: RPCResponse<any, object> = { type: 'rpc-response', id: msg.id, procedure: msg.procedure, request: msg, err, data };
+
+        if (p.send) {
+          p.send(response);
+          debug('%s: Sent RPC response: %O', this.name, response);
+        } else {
+          throw new IPCError('Cannot use proc: `send()` undefined.');
+        }
+      }
+    });
+
+    p.on('error', err => {
+      debug('%s: Encountered error with proc: %O', this.name, err);
+    });
+
+    debug('%s: RPC process initiated (pid: %d)', this.name, p.pid);
+  }
+
+  register<R extends RPCResponse<any, object>>(procedure: R['procedure'], fn: (args: R['request']['args']) => Promise<R['data']>): void {
+    this.responseProcedures.set(procedure, fn);
+  }
+
+  async call<R extends RPCResponse<any, object>>(procedure: R['procedure'], args: R['request']['args']): Promise<R['data']> {
+    const p = this.proc;
+
+    if (!p) {
+      throw new IPCError('Cannot call procedure: no proc started.');
+    }
+
+    const id = Math.random().toString(16).substring(2, 8);
+    const request: RPCRequest<any, any> = { type: 'rpc-request', id, procedure, args };
+
+    return new Promise<any>((resolve, reject) => {
+      const timer = setTimeout(() => {
+        reject(new IPCError(`Timeout of ${this.timeout}ms reached.`));
+      }, this.timeout);
+
+      const messageHandler = (msg: any) => {
+        if (isRPCResponse(msg) && msg.id === id) {
+          debug('%s: Received RPC response: %O', this.name, msg);
+          if (msg.err) {
+            reject(msg.err);
+          } else {
+            resolve(msg.data);
+          }
+
+          p.removeListener('message', messageHandler);
+          p.removeListener('disconnect', disconnectHandler);
+          clearTimeout(timer);
+        }
+      };
+
+      const disconnectHandler = () => {
+        reject(new IPCError('Unexpected disconnect. Rejecting call!'));
+        clearTimeout(timer);
+      };
+
+      p.on('message', messageHandler);
+      p.on('disconnect', disconnectHandler);
+
+      if (p.send) {
+        p.send(request);
+        debug('%s: Sent RPC request: %O', this.name, request);
+      } else {
+        reject(new IPCError('Cannot use proc: `send()` undefined.'));
+        clearTimeout(timer);
+      }
+    });
+  }
+
+  end(): void {
+    if (!this.proc) {
+      throw new IPCError(`RPC process not started.`);
+    }
+
+    this.proc.disconnect();
+    debug('%s: Disconnected', this.name);
+  }
+}
+
+export class RPCHost {
+  protected rpc: RPCProcess;
+
+  constructor(readonly modulePath: string, readonly args: ReadonlyArray<string>) {
+    this.rpc = new RPCProcess({ name: 'host' });
+  }
+
+  start(): void {
+    try {
+      fs.accessSync(this.modulePath, fs.constants.R_OK);
+    } catch (e) {
+      debug('Error during access check: %O', e);
+      throw new IPCError(`Module not accessible: ${this.modulePath}`);
+    }
+
+    const p = fork(this.modulePath, this.args, { stdio: ['ignore', 'ignore', 'ignore', 'ipc'] });
+    debug('RPC subprocess forked %o', [this.modulePath, ...this.args]);
+
+    this.rpc.start(p);
+  }
+
+  register<R extends RPCResponse<any, object>>(procedure: R['procedure'], fn: (args: R['request']['args']) => Promise<R['data']>): void {
+    this.rpc.register(procedure, fn);
+  }
+
+  async call<R extends RPCResponse<any, object>>(procedure: R['procedure'], args: R['request']['args']): Promise<R['data']> {
+    return this.rpc.call(procedure, args);
+  }
+
+  end(): void {
+    this.rpc.end();
+  }
+}
+
+function isRPCRequest(msg: any): msg is RPCRequest<any, any> {
+  return msg && msg.type === 'rpc-request' && typeof msg.procedure === 'string';
+}
+
+function isRPCResponse(msg: any): msg is RPCResponse<any, object> {
+  return msg && msg.type === 'rpc-response' && typeof msg.procedure === 'string';
+}
