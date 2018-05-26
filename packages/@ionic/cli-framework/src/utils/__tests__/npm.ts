6e94e5c3d932c14296ed03a52254ce1e035f1292
stupidawesome <stupidawesome@zerocents.com.au>
2018-05-26 12:54:59 +1000

    fix(@ionic/cli): Fix broken tests on Windows

--- packages/@ionic/cli-framework/src/utils/__tests__/npm.ts
+++ packages/@ionic/cli-framework/src/utils/__tests__/npm.ts
@@ -1,3 +1,4 @@
+import * as path from "path"
 import { compileNodeModulesPaths } from '../npm';
 
 describe('@ionic/cli-framework', () => {
@@ -6,18 +7,50 @@ describe('@ionic/cli-framework', () => {
 
     describe('compileNodeModulesPaths', () => {
 
-      it('should not accept a malformed path', () => {
-        expect(() => compileNodeModulesPaths('.')).toThrowError('. is not an absolute path');
+      describe('posix', () => {
+        const mock_path_posix = path.posix;
+        jest.resetModules();
+        jest.mock('path', () => mock_path_posix);
+
+        const npmLib = require('../npm');
+
+        it('should not accept a malformed path', () => {
+          expect(() => npmLib.compileNodeModulesPaths('.')).toThrowError('. is not an absolute path');
+        });
+
+        it('should compile an array of node_modules directories working backwards from a base directory', () => {
+          const result = npmLib.compileNodeModulesPaths('/some/dir');
+          expect(result).toEqual(['/some/dir/node_modules', '/some/node_modules', '/node_modules']);
+        });
+
+        it('should work for the root directory', () => {
+          const result = npmLib.compileNodeModulesPaths('/');
+          expect(result).toEqual(['/node_modules']);
+        });
+
       });
 
-      it('should compile an array of node_modules directories working backwards from a base directory', () => {
-        const result = compileNodeModulesPaths('/some/dir');
-        expect(result).toEqual(['/some/dir/node_modules', '/some/node_modules', '/node_modules']);
-      });
+      describe('windows', () => {
+        const mock_path_win32 = path.win32;
+        jest.resetModules();
+        jest.mock('path', () => mock_path_win32);
 
-      it('should work for the root directory', () => {
-        const result = compileNodeModulesPaths('/');
-        expect(result).toEqual(['/node_modules']);
+        const npmLib = require('../npm');
+
+        it('should not accept a malformed path', () => {
+          expect(() => npmLib.compileNodeModulesPaths('.')).toThrowError('. is not an absolute path');
+        });
+
+        it('should compile an array of node_modules directories working backwards from a base directory', () => {
+          const result = npmLib.compileNodeModulesPaths('\\some\\dir');
+          expect(result).toEqual(['\\some\\dir\\node_modules', '\\some\\node_modules', '\\node_modules']);
+        });
+
+        it('should work for the root directory', () => {
+          const result = npmLib.compileNodeModulesPaths('\\');
+          expect(result).toEqual(['\\node_modules']);
+        });
+
       });
 
     });
--- packages/@ionic/cli-utils/src/lib/project/ionic1/__tests__/index.ts
+++ packages/@ionic/cli-utils/src/lib/project/ionic1/__tests__/index.ts
@@ -1,3 +1,4 @@
+import * as path from "path"
 import { Ionic1Project } from '../';
 
 describe('@ionic/cli-utils', () => {
@@ -8,7 +9,7 @@ describe('@ionic/cli-utils', () => {
 
       it('should set directory attribute', async () => {
         const p = new Ionic1Project('/path/to/proj', 'file', {});
-        expect(p.directory).toEqual('/path/to/proj');
+        expect(p.directory).toEqual(path.resolve('/path/to/proj'));
       });
 
       describe('getSourceDir', () => {
@@ -16,13 +17,13 @@ describe('@ionic/cli-utils', () => {
         it('should use documentRoot, if set', async () => {
           const p = new Ionic1Project('/path/to/proj', 'file', {});
           spyOn(p, 'load').and.callFake(() => Promise.resolve({ documentRoot: 'some/dir' }));
-          expect(await p.getSourceDir()).toEqual('/path/to/proj/some/dir');
+          expect(await p.getSourceDir()).toEqual(path.resolve('/path/to/proj/some/dir'));
         });
 
         it('should default to www', async () => {
           const p = new Ionic1Project('/path/to/proj', 'file', {});
           const result = await p.getSourceDir();
-          expect(result).toEqual('/path/to/proj/www');
+          expect(result).toEqual(path.resolve('/path/to/proj/www'));
         });
 
       });
