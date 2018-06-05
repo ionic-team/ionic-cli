cbd9b00c5d255e7be0807c4d81c41a42a55d5e44
Daniel Imhoff <dwieeb@gmail.com>
2018-06-05 16:24:46 -0500

    refactor(terminal): make ci vars readonly

--- packages/@ionic/cli-framework/src/utils/terminal.ts
+++ packages/@ionic/cli-framework/src/utils/terminal.ts
@@ -6,7 +6,7 @@ const debug = Debug('ionic:cli-framework:utils:terminal');
  * These environment variables work for: Travis CI, CircleCI, Gitlab CI,
  * AppVeyor, CodeShip, Jenkins, TeamCity, Bitbucket Pipelines, AWS CodeBuild
  */
-export const CI_ENVIRONMENT_VARIABLES = ['CI', 'BUILD_ID', 'BUILD_NUMBER', 'BITBUCKET_COMMIT', 'CODEBUILD_BUILD_ARN'];
+export const CI_ENVIRONMENT_VARIABLES: ReadonlyArray<string> = ['CI', 'BUILD_ID', 'BUILD_NUMBER', 'BITBUCKET_COMMIT', 'CODEBUILD_BUILD_ARN'];
 export const CI_ENVIRONMENT_VARIABLES_DETECTED = CI_ENVIRONMENT_VARIABLES.filter(v => !!process.env[v]);
 
 if (CI_ENVIRONMENT_VARIABLES_DETECTED.length > 0) {
