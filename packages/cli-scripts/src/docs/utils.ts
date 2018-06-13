e26d73df70b6ac78bb1dc59f7d85066ac4d53f9c
Daniel Imhoff <dwieeb@gmail.com>
2018-06-13 13:32:13 -0500

    chore(deps): update @types/ansi-styles

--- packages/cli-scripts/src/docs/utils.ts
+++ packages/cli-scripts/src/docs/utils.ts
@@ -1,6 +1,11 @@
 import * as ansiStyle from 'ansi-styles';
 import * as escapeStringRegexp from 'escape-string-regexp';
 
+interface CodePair {
+  open: string;
+  close: string;
+}
+
 export function links2md(str: string) {
   str = str.replace(/((http|https):\/\/(\w+:{0,1}\w*@)?([^\s\*\)`]+)(\/|\/([\w#!:.?+=&%@!\-\/]))?)/g, '[$1]($1)');
   str = str.replace(/\[(\d+)\]/g, '\\\[$1\\\]');
@@ -15,7 +20,7 @@ export function ansi2md(str: string) {
   return str;
 }
 
-function convertAnsiToMd(str: string, style: ansiStyle.EscapeCodePair, md: ansiStyle.EscapeCodePair) {
+function convertAnsiToMd(str: string, style: CodePair, md: CodePair) {
   str = str.replace(new RegExp(escapeStringRegexp(style.open) + '(.*?)' + escapeStringRegexp(style.close), 'g'), md.open + '$1' + md.close);
   return str;
 }
