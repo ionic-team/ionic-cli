6fc7965e47fff9511c196fbbe86f567ed90b6acf
Daniel Imhoff <dwieeb@gmail.com>
2018-09-26 10:46:29 -0500

    wip

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,13 +1,3 @@
 module.exports = {
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json"
-  ],
+  preset: 'ts-jest',
 };
