c1710037b4cc954ad2fd793568019c772b1cc587
Daniel Imhoff <dwieeb@gmail.com>
2018-10-15 14:30:57 -0500

    chore: changes to make it compile

--- packages/ionic/src/commands/ssh/generate.ts
+++ packages/ionic/src/commands/ssh/generate.ts
@@ -98,7 +98,7 @@ export class SSHGenerateCommand extends SSHBaseCommand implements CommandPreRun 
     );
 
     await this.env.close();
-    const shellOptions = { stdio: 'inherit', showCommand: false, showExecution: false, showError: false };
+    const shellOptions: any = { stdio: 'inherit', showCommand: false, showExecution: false, showError: false };
     await this.env.shell.run('ssh-keygen', ['-q', '-t', String(options['type']), '-b', String(bits), '-C', String(annotation), '-f', keyPath], shellOptions);
     await this.env.open();
 
