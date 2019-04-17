79480b9eef0ed9c39772a60ad3828da8f851d684
Daniel Imhoff <dwieeb@gmail.com>
2019-04-17 17:41:31 -0500

    feat: include path to shell in terminal info

--- packages/@ionic/utils-terminal/src/index.ts
+++ packages/@ionic/utils-terminal/src/index.ts
@@ -9,20 +9,41 @@ const debug = Debug('ionic:utils-terminal');
 export const CI_ENVIRONMENT_VARIABLES: ReadonlyArray<string> = ['CI', 'BUILD_ID', 'BUILD_NUMBER', 'BITBUCKET_COMMIT', 'CODEBUILD_BUILD_ARN'];
 export const CI_ENVIRONMENT_VARIABLES_DETECTED = CI_ENVIRONMENT_VARIABLES.filter(v => !!process.env[v]);
 
+function getShell(): string {
+  if (process.env.SHELL) {
+    return process.env.SHELL;
+  }
+
+  if (process.platform === 'darwin') {
+    return '/bin/bash';
+  }
+
+  if (process.platform === 'win32') {
+    return process.env.COMSPEC ? process.env.COMSPEC : 'cmd.exe';
+  }
+
+  return '/bin/sh';
+}
+
 if (CI_ENVIRONMENT_VARIABLES_DETECTED.length > 0) {
   debug(`Environment variables for CI detected: ${CI_ENVIRONMENT_VARIABLES_DETECTED.join(', ')}`);
 }
 
 export interface TerminalInfo {
   /**
-   * Whether the terminal is an interactive TTY or not.
-   */
-  readonly tty: boolean;
-
-  /**
    * Whether this is in CI or not.
    */
   readonly ci: boolean;
+
+  /**
+   * Path to the user's shell program.
+   */
+  readonly shell: string;
+
+  /**
+   * Whether the terminal is an interactive TTY or not.
+   */
+  readonly tty: boolean;
 
   /**
    * Whether this is a Windows shell or not.
@@ -31,8 +52,9 @@ export interface TerminalInfo {
 }
 
 export const TERMINAL_INFO: TerminalInfo = Object.freeze({
-  tty: Boolean(process.stdin.isTTY && process.stdout.isTTY && process.stderr.isTTY),
   ci: CI_ENVIRONMENT_VARIABLES_DETECTED.length > 0,
+  shell: getShell(),
+  tty: Boolean(process.stdin.isTTY && process.stdout.isTTY && process.stderr.isTTY),
   windows: process.platform === 'win32' || (
     process.env.OSTYPE && /^(msys|cygwin)$/.test(process.env.OSTYPE) ||
     process.env.MSYSTEM && /^MINGW(32|64)$/.test(process.env.MSYSTEM) ||
