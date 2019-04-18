e63b74a4d3dea6b711fcc92c68088cff0912112d
Daniel Imhoff <dwieeb@gmail.com>
2019-04-17 19:22:11 -0500

    fix(open): fix unbound listener

--- packages/ionic/src/lib/open.ts
+++ packages/ionic/src/lib/open.ts
@@ -10,7 +10,7 @@ export async function open(target: string, options: OpenOptions = {}): Promise<v
   const o = await import ('open');
   const p = await o(target, { ...options, wait: false });
   const e = (err: Error) => debug('Error during open: %O', err);
-  const n = p.on;
+  const n = p.on.bind(p);
 
   n('error', err => e(err));
 }
