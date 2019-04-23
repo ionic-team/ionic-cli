eadcba0ed3079301219e1697658c1be4e51fd0ce
Daniel Imhoff <dwieeb@gmail.com>
2019-04-22 19:30:23 -0500

    feat(cordova): use `native-run` by default
    native-run is a utility built by Ionic for running native binaries on
    iOS and Android devices and simulators/emulators. See the GitHub repo:
    https://github.com/ionic-team/native-run
    
    native-run only works for iOS and Android. If a different platform is
    used, Cordova is used to run the app.
    
    BREAKING CHANGE: `ionic cordova run/emulate` no longer use Cordova to
    deploy apps by default. `cordova run`, which builds and deploys the app,
    is no longer used. Instead, `cordova build` is used to build the app and
    then it is deployed to devices using `native-run`. The old behavior is
    available by specifying `--no-native-run`. When using Cordova to run,
    unless you manually forward ports, you will need to also specify
    `--address=0.0.0.0` (or any host accessible externally).

--- packages/ionic/src/lib/integrations/cordova/project.ts
+++ packages/ionic/src/lib/integrations/cordova/project.ts
@@ -2,6 +2,10 @@ import { filter } from '@ionic/utils-array';
 import { readdirSafe, statSafe } from '@ionic/utils-fs';
 import * as path from 'path';
 
+const CORDOVA_ANDROID_PACKAGE_PATH = 'platforms/android/app/build/outputs/apk/';
+const CORDOVA_IOS_SIMULATOR_PACKAGE_PATH = 'platforms/ios/build/emulator';
+const CORDOVA_IOS_DEVICE_PACKAGE_PATH = 'platforms/ios/build/device';
+
 export async function getPlatforms(projectDir: string): Promise<string[]> {
   const platformsDir = path.resolve(projectDir, 'platforms');
   const contents = await readdirSafe(platformsDir);
@@ -11,4 +15,17 @@ export async function getPlatforms(projectDir: string): Promise<string[]> {
   });
 
   return platforms;
+}
+
+export async function getPackagePath(appName: string, platform: string, emulator: boolean): Promise<string> {
+  if (platform === 'android') {
+    // TODO: don't hardcode this/support multiple build paths (ex: multiple arch builds)
+    // use app/build/outputs/apk/debug/output.json?
+    return path.join(CORDOVA_ANDROID_PACKAGE_PATH, 'debug', 'app-debug.apk');
+  }
+  if (platform === 'ios' && emulator) {
+    return path.join(CORDOVA_IOS_SIMULATOR_PACKAGE_PATH, `${appName}.app`);
+  } else {
+    return path.join(CORDOVA_IOS_DEVICE_PACKAGE_PATH, `${appName}.ipa`);
+  }
 }
