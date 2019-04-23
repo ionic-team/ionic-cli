3570ed3d933b4fca77fbe7fad6090506898f2df8
Daniel Imhoff <dwieeb@gmail.com>
2019-04-23 16:53:08 -0500

    chore(color): revert Ionic colors for now
    Looks too much like selected text.

--- packages/ionic/src/lib/color.ts
+++ packages/ionic/src/lib/color.ts
@@ -2,7 +2,7 @@ import { Colors, DEFAULT_COLORS, HelpColors } from '@ionic/cli-framework';
 import chalk from 'chalk';
 
 const HELP_COLORS: Partial<HelpColors> = {
-  title: chalk.bold.rgb(0xFF, 0xFF, 0xFF).bgRgb(0x30, 0x75, 0xFF),
+  title: chalk.bold,
 };
 
 export const COLORS: Colors = { ...DEFAULT_COLORS, help: { ...DEFAULT_COLORS.help, ...HELP_COLORS } };
