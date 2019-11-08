315d621bd99df26360e2607045f73bbdfe317c9d
Tim Lancina <tim@ionic.io>
2019-11-08 13:13:42 -0800

    fix(login): don't doubly encode SSO login URL params
    Fixes https://github.com/ionic-team/ionic-cli/issues/4208.

--- packages/ionic/src/lib/open.ts
+++ packages/ionic/src/lib/open.ts
@@ -4,11 +4,16 @@ const debug = Debug('ionic:lib:open');
 
 export interface OpenUrlOptions {
   app?: string | readonly string[];
+  encode?: boolean;
 }
 
 export async function openUrl(target: string, options: OpenUrlOptions = {}): Promise<void> {
   const o = await import ('open');
-  const p = await o(target, { ...options, wait: false, url: true });
+  // an option named 'url' doesn't make much sense for an function named 'openUrl', but encode
+  // corresponds to `open`'s 'url' option
+  const opts = { ...options, url: options.encode };
+  delete opts['encode'];
+  const p = await o(target, { wait: false, url: true, ...opts });
   const e = (err: Error) => debug('Error during open: %O', err);
   const n = p.on.bind(p);
 
