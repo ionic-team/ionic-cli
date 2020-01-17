d17c1bdc20cb962f386d7ccf15db24252dd77425
Daniel Imhoff <dwieeb@gmail.com>
2020-01-17 13:13:59 -0800

    chore(lab): switch to using internal module for JSXBase

--- packages/@ionic/lab/src/stencil/declarations.d.ts
+++ packages/@ionic/lab/src/stencil/declarations.d.ts
@@ -1,6 +1,6 @@
 export {}; // https://github.com/Microsoft/TypeScript/issues/17736
 
-declare module '@stencil/core/dist/declarations/jsx' {
+declare module '@stencil/core/internal' {
   namespace JSXBase {
     interface IframeHTMLAttributes<T> extends HTMLAttributes<T> {
       allow?: string;
