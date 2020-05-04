f4f05ffe4649a66f4c7961ee09bea97343d0e4f3
Carl Poole <mail@carlpoole.com>
2020-05-04 18:02:22 -0500

    docs(capacitor): add note to explain serving to emulators and to lan/devices (#4420)

--- packages/@ionic/cli/src/commands/capacitor/run.ts
+++ packages/@ionic/cli/src/commands/capacitor/run.ts
@@ -17,8 +17,8 @@ export class RunCommand extends CapacitorCommand implements CommandPreRun {
     const exampleCommands = [
       '',
       'android',
-      'android -l',
-      'ios --livereload',
+      'android -l --external',
+      'ios --livereload --external',
       'ios --livereload-url=http://localhost:8100',
     ].sort();
 
@@ -88,6 +88,8 @@ ${input('ionic capacitor run')} will do the following:
 - Perform ${input('ionic build')} (or run the dev server from ${input('ionic serve')} with the ${input('--livereload')} option)
 - Copy web assets into the specified native platform
 - Open the IDE for your native project (Xcode for iOS, Android Studio for Android)
+
+When using the ${input('--livereload')} option and need to serve to your LAN, a device, or an emulator, use the ${input('--external')} option also. Otherwise, the web view tries to access ${input('localhost')}.
 
 Once the web assets and configuration are copied into your native project, the app can run on devices and emulators/simulators using the native IDE. Unfortunately, programmatically building and launching the native project is not yet supported.
 
