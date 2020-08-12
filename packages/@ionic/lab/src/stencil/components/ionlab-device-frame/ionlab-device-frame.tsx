95289cf41b45070b13cf1230c0051956ded188f5
Brandy Carney <brandy@ionic.io>
2020-08-12 16:13:09 -0400

    fix(lab): update statusbar for ios and md

--- packages/@ionic/lab/src/stencil/components/ionlab-device-frame/ionlab-device-frame.tsx
+++ packages/@ionic/lab/src/stencil/components/ionlab-device-frame/ionlab-device-frame.tsx
@@ -36,7 +36,13 @@ export class DeviceFrame {
     return [
       <h2><i class={ ['icon', this.icon].join(' ') } />{ this.platformName }</h2>,
       <div class="frame-container">
-        <div class="statusbar" />
+        <div class="statusbar">
+          <svg class="statusbar-md" viewBox="0 0 1384.3 40.3">
+            <path class="st0" d="M1343 5l18.8 32.3c.8 1.3 2.7 1.3 3.5 0L1384 5c.8-1.3-.2-3-1.7-3h-37.6c-1.5 0-2.5 1.7-1.7 3z"/>
+            <circle class="st0" cx="1299" cy="20.2" r="20"/>
+            <path class="st0" d="M1213 1.2h30c2.2 0 4 1.8 4 4v30c0 2.2-1.8 4-4 4h-30c-2.2 0-4-1.8-4-4v-30c0-2.3 1.8-4 4-4zM16 4.2h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H16c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/>
+          </svg>
+        </div>
         { !this.loaded && !this.error ? <sk-fading-circle /> : null }
         { this.error ? <div class="load-error"><h3>Load Timeout</h3><p>Still trying...</p></div> : null }
         <iframe src={ this.url } onLoad={ event => this.loadedHandler(event) } allow="geolocation; microphone; camera; midi; encrypted-media"/>
