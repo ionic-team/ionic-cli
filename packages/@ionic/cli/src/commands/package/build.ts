7713bae699f3fcd253702a1f30f233b5b22890b4
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2020-08-20 15:18:55 -0500

    fix(appflow): fix an issue where a network connectivity issue could cause an appflow build to fail incorrectly

--- packages/@ionic/cli/src/commands/package/build.ts
+++ packages/@ionic/cli/src/commands/package/build.ts
@@ -340,17 +340,29 @@ This can be used only together with build type ${input('release')} for Android a
     const ws = this.env.log.createWriteStream(LOGGER_LEVELS.INFO, false);
 
     let isCreatedMessage = false;
+    let errorsEncountered = 0;
     while (!(build && (build.state === 'success' || build.state === 'failed'))) {
-      await sleep(5000);
-      build = await this.getPackageBuild(appflowId, buildId, token);
-      if (build && build.state === 'created' && !isCreatedMessage) {
-        ws.write(chalk.yellow('Concurrency limit reached: build will start as soon as other builds finish.'));
-        isCreatedMessage = true;
-      }
-      const trace = build.job.trace;
-      if (trace.length > start) {
-        ws.write(trace.substring(start));
-        start = trace.length;
+      try {
+        await sleep(5000);
+        build = await this.getPackageBuild(appflowId, buildId, token);
+        if (build && build.state === 'created' && !isCreatedMessage) {
+          ws.write(chalk.yellow('Concurrency limit reached: build will start as soon as other builds finish.'));
+          isCreatedMessage = true;
+        }
+        const trace = build.job.trace;
+        if (trace.length > start) {
+          ws.write(trace.substring(start));
+          start = trace.length;
+        }
+        errorsEncountered = 0;
+      } catch (e) {
+        // Retry up to 3 times in the case of an error.
+        errorsEncountered++;
+        if (errorsEncountered >= 3) {
+          ws.write(chalk.red(`Encountered ${errorsEncountered} errors in a row. Job will now fail.`));
+          throw e;
+        }
+        ws.write(chalk.yellow(`Encountered error: ${e} while fetching build data retrying.`));
       }
     }
     ws.end();
