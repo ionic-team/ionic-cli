5905cc1a9cd75b1a2bb8343da2fbc7146322ad28
Daniel Imhoff <dwieeb@gmail.com>
2020-09-24 14:21:14 -0700

    fix(cordova): handle output-metadata.json files during android run

--- packages/@ionic/cli/src/lib/integrations/cordova/project.ts
+++ packages/@ionic/cli/src/lib/integrations/cordova/project.ts
@@ -25,28 +25,34 @@ export async function getPlatforms(projectDir: string): Promise<string[]> {
   return platforms;
 }
 
-export async function getAndroidBuildOutputJson(p: string): Promise<LegacyAndroidBuildOutputEntry[] | AndroidBuildOutput> {
-  try {
-    const json = await readJson(p);
+export async function getAndroidBuildOutputJson(paths: string[]): Promise<LegacyAndroidBuildOutputEntry[] | AndroidBuildOutput> {
+  for (const p of paths) {
+    try {
+      const json = await readJson(p);
 
-    if (isAndroidBuildOutputFile(json)) {
-      return json;
-    } else if (isLegacyAndroidBuildOutputFile(json)) {
-      return json;
-    } else {
-      debug('Output file does not match expected format: %O', json);
+      if (isAndroidBuildOutputFile(json)) {
+        return json;
+      } else if (isLegacyAndroidBuildOutputFile(json)) {
+        return json;
+      } else {
+        debug('Output file does not match expected format: %O', json);
+      }
+    } catch (e) {
+      debug('Error parsing file %O: %O', p, e);
     }
-  } catch (e) {
-    debug('Error parsing file %O: %O', p, e);
   }
 
-  throw new FatalException(`Could not parse build output file: ${p}`);
+  throw new FatalException(
+    `Could not find or parse valid build output file.\n` +
+    `Tried the following paths:\n` +
+    `- ${paths.join('\n- ')}`
+  );
 }
 
 export async function getAndroidPackageFilePath(root: string, { release = false }: GetPackagePathOptions): Promise<string> {
   const outputPath = path.resolve(root, CORDOVA_ANDROID_PACKAGE_PATH, release ? 'release' : 'debug');
-  const outputJsonPath = path.resolve(outputPath, 'output.json');
-  const outputJson = await getAndroidBuildOutputJson(outputJsonPath);
+  const outputJsonPaths = ['output.json', 'output-metadata.json'].map(p => path.resolve(outputPath, p));
+  const outputJson = await getAndroidBuildOutputJson(outputJsonPaths);
 
   const p = 'elements' in outputJson
     ? outputJson.elements[0].outputFile
