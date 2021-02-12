cee620baff2515115393d882b88f6a99e3fefd9b
Devin Shoemaker <devinshoe@gmail.com>
2021-02-12 14:20:44 -0600

    feat(appflow): support Capacitor only apps with `ionic deploy manifest` (#4641)

--- packages/@ionic/cli/src/commands/deploy/manifest.ts
+++ packages/@ionic/cli/src/commands/deploy/manifest.ts
@@ -1,16 +1,25 @@
 import { MetadataGroup } from '@ionic/cli-framework';
+import { LOGGER_LEVELS } from '@ionic/cli-framework-output';
 import { map } from '@ionic/utils-array';
-import { readdirp, stat, writeFile } from '@ionic/utils-fs';
+import { pathExists, readdirp, stat, writeFile } from '@ionic/utils-fs';
 import { prettyPath } from '@ionic/utils-terminal';
 import * as crypto from 'crypto';
 import * as fs from 'fs';
+import lodash = require('lodash');
 import * as path from 'path';
+import * as Debug from 'debug';
 
 import { CommandMetadata } from '../../definitions';
 import { input } from '../../lib/color';
 import { FatalException } from '../../lib/errors';
+import { prependNodeModulesBinToPath, Shell } from '../../lib/shell';
+import { createDefaultLoggerHandlers, Logger } from '../../lib/utils/logger';
 
+import { CapacitorCLIConfig, CapacitorConfig, CapacitorJSONConfig } from './capacitor';
 import { DeployCoreCommand } from './core';
+
+const debug = Debug('ionic:commands:deploy:manifest');
+const CAPACITOR_CONFIG_JSON_FILE = 'capacitor.config.json';
 
 interface DeployManifestItem {
   href: string;
@@ -20,21 +29,29 @@ interface DeployManifestItem {
 
 export class DeployManifestCommand extends DeployCoreCommand {
   async getMetadata(): Promise<CommandMetadata> {
+    // This command is set as type 'global' in order to support Capacitor apps without an ionic.config.json
     return {
       name: 'manifest',
-      type: 'project',
+      type: 'global',
       summary: 'Generates a manifest file for the deploy service from a built app directory',
       groups: [MetadataGroup.PAID],
     };
   }
 
   async run(): Promise<void> {
-    if (!this.project) {
+    const capacitorConfig = await this.getCapacitorConfig();
+    if (!this.project && !capacitorConfig) {
       throw new FatalException(`Cannot run ${input('ionic deploy manifest')} outside a project directory.`);
     }
-    await this.requireNativeIntegration();
 
-    const buildDir = await this.project.getDistDir();
+    let buildDir: string;
+    if (this.project) {
+      await this.requireNativeIntegration();
+      buildDir = await this.project.getDistDir();
+    } else {
+      buildDir = capacitorConfig!.webDir ? capacitorConfig!.webDir : 'www';
+    }
+
     const manifest = await this.getFilesAndSizesAndHashesForGlobPattern(buildDir);
 
     const manifestPath = path.resolve(buildDir, 'pro-manifest.json');
@@ -83,4 +100,55 @@ export class DeployManifestCommand extends DeployCoreCommand {
       })
       .join(' ');
   }
+
+  private getCapacitorConfigJsonPath(): string {
+    return path.resolve(this.env.ctx.execPath, CAPACITOR_CONFIG_JSON_FILE);
+  }
+
+  private getCapacitorCLIConfig = lodash.memoize(async (): Promise<CapacitorCLIConfig | undefined> => {
+    // I had to create a new shell to force prependNodeModulesBinToPath.
+    // If ionic.config.json is not present, then this.env.shell will not implement this, and the Capacitor command will fail.
+    const args = ['config', '--json'];
+    const log = new Logger({
+      level: LOGGER_LEVELS.INFO,
+      handlers: createDefaultLoggerHandlers(),
+    });
+    const shell = new Shell({ log }, { alterPath: p => { return prependNodeModulesBinToPath(this.env.ctx.execPath, p)} });
+
+    debug('Getting config with Capacitor CLI: %O', args);
+
+    const output = await shell.cmdinfo('capacitor', args);
+
+    if (!output) {
+      debug('Could not get config from Capacitor CLI (probably old version)');
+      return;
+    }
+
+    return JSON.parse(output);
+  });
+
+  private getCapacitorConfig = lodash.memoize(async (): Promise<CapacitorConfig | undefined> => {
+    const cli = await this.getCapacitorCLIConfig();
+
+    if (cli) {
+      debug('Loaded Capacitor config!');
+      return cli.app.extConfig;
+    }
+
+    // fallback to reading capacitor.config.json if it exists
+    const confPath = this.getCapacitorConfigJsonPath();
+
+    if (!(await pathExists(confPath))) {
+      debug('Capacitor config file does not exist at %O', confPath);
+      debug('Failed to load Capacitor config');
+      return;
+    }
+
+    const conf = new CapacitorJSONConfig(confPath);
+    const extConfig = conf.c;
+
+    debug('Loaded Capacitor config!');
+
+    return extConfig;
+  });
 }
