a55d5125d6810739cc96573f06c7d9ef44b684e6
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-03-13 13:03:34 -0400

    sync

--- packages/@ionic/lab/src/stencil/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-export {}; // https://github.com/Microsoft/TypeScript/issues/17736
-
-declare module '@stencil/core/internal' {
-  namespace JSXBase {
-    interface IframeHTMLAttributes<T> extends HTMLAttributes<T> {
-      allow?: string;
-    }
-  }
-}
