94e9fa2a2146883ad857d2acad71b7605e3687d0
Mike Hartington <mhartington@users.noreply.github.com>
2023-03-20 11:25:09 -0400

    feat(): add vite support for react and vue (#4966)
    * feat(): add vite support for react and vue
    This adds two additional project types, vue-vite and react-vite. This is
    to support the plan for migrating the react and vue stater projects to
    vite while also maintaining backwards compat for older react-scripts
    based projects and vue-cli based projects.
    
    * fix(): update based on feedback

--- packages/@ionic/cli/src/constants.ts
+++ packages/@ionic/cli/src/constants.ts
@@ -7,7 +7,7 @@ import { ProjectType } from './definitions';
 export const ASSETS_DIRECTORY = path.resolve(__dirname, 'assets');
 
 export const PROJECT_FILE = process.env['IONIC_CONFIG_FILE'] ?? 'ionic.config.json';
-export const PROJECT_TYPES: ProjectType[] = ['angular', 'react', 'vue', 'ionic-angular', 'ionic1', 'custom'];
+export const PROJECT_TYPES: ProjectType[] = ['angular', 'react', 'vue', 'ionic-angular', 'ionic1', 'custom', 'vue-vite', 'react-vite'];
 export const LEGACY_PROJECT_TYPES: ProjectType[] = ['ionic-angular', 'ionic1'];
 export const MODERN_PROJECT_TYPES: ProjectType[] = lodash.difference(PROJECT_TYPES, LEGACY_PROJECT_TYPES);
 
---
+++ packages/@ionic/cli/src/lib/project/react-vite/index.ts
@@ -0,0 +1,80 @@
+import * as chalk from 'chalk';
+import * as Debug from 'debug';
+import * as lodash from 'lodash';
+import * as path from 'path';
+
+import { Project } from '../';
+import { InfoItem } from '../../../definitions';
+import { RunnerNotFoundException } from '../../errors';
+
+const debug = Debug('ionic:lib:project:react');
+
+export class ReactViteProject extends Project {
+  readonly type: 'react' = 'react';
+
+  async getInfo(): Promise<InfoItem[]> {
+    const [
+      [ionicReact, ionicReactPath],
+    ] = await Promise.all([
+      this.getPackageJson('@ionic/react'),
+    ]);
+
+    return [
+      ...(await super.getInfo()),
+      {
+        group: 'ionic',
+        name: 'Ionic Framework',
+        key: 'framework',
+        value: ionicReact ? `@ionic/react ${ionicReact.version}` : 'not installed',
+        path: ionicReactPath,
+      },
+    ];
+  }
+
+  /**
+   * We can't detect React project types. We don't know what they look like!
+   */
+  async detected() {
+    try {
+      const pkg = await this.requirePackageJson();
+      const deps = lodash.assign({}, pkg.dependencies, pkg.devDependencies);
+
+      if (typeof deps['@ionic/react'] === 'string') {
+        debug(`${chalk.bold('@ionic/react')} detected in ${chalk.bold('package.json')}`);
+        return true;
+      }
+    } catch (e) {
+      // ignore
+    }
+
+    return false;
+  }
+
+  async getDefaultDistDir(): Promise<string> {
+    return 'dist';
+  }
+
+  async requireBuildRunner(): Promise<import('./build').ReactViteBuildRunner> {
+    const { ReactViteBuildRunner } = await import('./build');
+    const deps = { ...this.e, project: this };
+    return new ReactViteBuildRunner(deps);
+  }
+
+  async requireServeRunner(): Promise<import('./serve').ReactViteServeRunner> {
+    const { ReactViteServeRunner } = await import('./serve');
+    const deps = { ...this.e, project: this };
+    return new ReactViteServeRunner(deps);
+  }
+
+  async requireGenerateRunner(): Promise<never> {
+    throw new RunnerNotFoundException(
+      `Cannot perform generate for React projects.\n` +
+      `Since you're using the ${chalk.bold('React')} project type, this command won't work. The Ionic CLI doesn't know how to generate framework components for React projects.`
+    );
+  }
+
+  setPrimaryTheme(themeColor: string): Promise<void> {
+    const themePath = path.join(this.directory, 'src', 'theme', 'variables.css');
+    return this.writeThemeColor(themePath, themeColor);
+  }
+}
