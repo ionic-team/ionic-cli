b73ba0d9197e089f1eec1a0c7c65e867a1f09611
Mike Hartington <mhartington@users.noreply.github.com>
2023-03-20 11:25:09 -0400

    feat(): add vite support for react and vue (#4966)
    * feat(): add vite support for react and vue
    This adds two additional project types, vue-vite and react-vite. This is
    to support the plan for migrating the react and vue stater projects to
    vite while also maintaining backwards compat for older react-scripts
    based projects and vue-cli based projects.
    
    * fix(): update based on feedback

---
+++ packages/@ionic/cli/src/lib/project/vue-vite/index.ts
@@ -0,0 +1,80 @@
+import * as chalk from 'chalk';
+import * as Debug from 'debug';
+import * as lodash from 'lodash';
+import * as path from 'path';
+
+import { Project } from '../';
+import { InfoItem } from '../../../definitions';
+import { RunnerNotFoundException } from '../../errors';
+
+const debug = Debug('ionic:lib:project:vue');
+
+export class VueViteProject extends Project {
+  readonly type: 'vue' = 'vue';
+
+  async getInfo(): Promise<InfoItem[]> {
+    const [
+      [ionicVuePkg, ionicVuePkgPath],
+    ] = await Promise.all([
+      this.getPackageJson('@ionic/vue'),
+    ]);
+
+    return [
+      ...(await super.getInfo()),
+      {
+        group: 'ionic',
+        name: 'Ionic Framework',
+        key: 'framework',
+        value: ionicVuePkg ? `@ionic/vue ${ionicVuePkg.version}` : 'not installed',
+        path: ionicVuePkgPath,
+      },
+    ];
+  }
+
+  /**
+   * We can't detect Vue project types. We don't know what they look like!
+   */
+  async detected() {
+    try {
+      const pkg = await this.requirePackageJson();
+      const deps = lodash.assign({}, pkg.dependencies, pkg.devDependencies);
+
+      if (typeof deps['@ionic/vue'] === 'string') {
+        debug(`${chalk.bold('@ionic/vue')} detected in ${chalk.bold('package.json')}`);
+        return true;
+      }
+    } catch (e) {
+      // ignore
+    }
+
+    return false;
+  }
+
+  async getDefaultDistDir(): Promise<string> {
+    return 'dist';
+  }
+
+  async requireBuildRunner(): Promise<import('./build').VueViteBuildRunner> {
+    const { VueViteBuildRunner } = await import('./build');
+    const deps = { ...this.e, project: this };
+    return new VueViteBuildRunner(deps);
+  }
+
+  async requireServeRunner(): Promise<import('./serve').VueServeRunner> {
+    const { VueServeRunner } = await import('./serve');
+    const deps = { ...this.e, project: this };
+    return new VueServeRunner(deps);
+  }
+
+  async requireGenerateRunner(): Promise<never> {
+    throw new RunnerNotFoundException(
+      `Cannot perform generate for Vue projects.\n` +
+      `Since you're using the ${chalk.bold('Vue')} project type, this command won't work. The Ionic CLI doesn't know how to generate framework components for Vue projects.`
+    );
+  }
+
+  setPrimaryTheme(themeColor: string): Promise<void> {
+    const themePath = path.join(this.directory, 'src', 'theme', 'variables.css');
+    return this.writeThemeColor(themePath, themeColor);
+  }
+}
